7f34d1a3733f2ba697208651cb202e85df4e0360
Dylan Marsh <dylan@knoware.co>
2021-01-13 09:48:44 -0500

    Fix redirect for virtual tour

--- middleware/redirects.js
+++ middleware/redirects.js
@@ -4,8 +4,6 @@ export default function(context) {
     if (context.req) {
         if (context.req.url === '/training-forum') {
             context.redirect('https://training.assessments24x7.com');
-        } else if (context.req.url === '/virtualtour.asp') {
-            context.redirect('https://vault.assessments24x7.com/virtualtour.asp');
         } else if (context.req.url === '/webinar-archive.asp') {
             context.redirect('https://vault.assessments24x7.com/webinar-archive.asp');
         } else if (context.req.url == '/reports/SampleTRScoringReport.pdf') {
