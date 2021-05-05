33d6c107cf407a8525b85a3694661a41b0b7e3c0
Dylan Marsh <dylantmarsh@Dylans-MacBook-Pro.local>
2021-05-05 13:46:52 -0400

    Removing old sample reports

--- pages/assessments/disc.vue
+++ pages/assessments/disc.vue
@@ -53,43 +53,7 @@
                             <h2>DISC Sample Reports</h2>
                             <a href="/reports/DISCselfSampleReport.pdf" target="_blank" rel="noopener">
                                 <div class="sample">
-                                    <img src="~assets/pdf-icon.svg"> DISC Self
-                                    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 185.343 185.343">
-                                        <path style="fill:#05bbff;" d="M51.707,185.343c-2.741,0-5.493-1.044-7.593-3.149c-4.194-4.194-4.194-10.981,0-15.175
-                                            l74.352-74.347L44.114,18.32c-4.194-4.194-4.194-10.987,0-15.175c4.194-4.194,10.987-4.194,15.18,0l81.934,81.934
-                                            c4.194,4.194,4.194,10.987,0,15.175l-81.934,81.939C57.201,184.293,54.454,185.343,51.707,185.343z"
-                                        />
-                                    </svg>
-                                </div>
-                            </a>
-
-                            <a href="/reports/DISCLeadershipSampleReport.pdf" target="_blank" rel="noopener">
-                                <div class="sample">
-                                    <img src="~assets/pdf-icon.svg"> DISC Leadership
-                                    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 185.343 185.343">
-                                        <path style="fill:#05bbff;" d="M51.707,185.343c-2.741,0-5.493-1.044-7.593-3.149c-4.194-4.194-4.194-10.981,0-15.175
-                                            l74.352-74.347L44.114,18.32c-4.194-4.194-4.194-10.987,0-15.175c4.194-4.194,10.987-4.194,15.18,0l81.934,81.934
-                                            c4.194,4.194,4.194,10.987,0,15.175l-81.934,81.939C57.201,184.293,54.454,185.343,51.707,185.343z"
-                                        />
-                                    </svg>
-                                </div>
-                            </a>
-
-                            <a href="/reports/SalesDISCselfSampleReport.pdf" target="_blank" rel="noopener">
-                                <div class="sample">
-                                    <img src="~assets/pdf-icon.svg"> DISC Sales
-                                    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 185.343 185.343">
-                                        <path style="fill:#05bbff;" d="M51.707,185.343c-2.741,0-5.493-1.044-7.593-3.149c-4.194-4.194-4.194-10.981,0-15.175
-                                            l74.352-74.347L44.114,18.32c-4.194-4.194-4.194-10.987,0-15.175c4.194-4.194,10.987-4.194,15.18,0l81.934,81.934
-                                            c4.194,4.194,4.194,10.987,0,15.175l-81.934,81.939C57.201,184.293,54.454,185.343,51.707,185.343z"
-                                        />
-                                    </svg>
-                                </div>
-                            </a>
-
-                            <a href="/reports/DISCService.pdf" target="_blank" rel="noopener">
-                                <div class="sample">
-                                    <img src="~assets/pdf-icon.svg"> DISC Service
+                                    <img src="~assets/pdf-icon.svg"> DISC Sample Report
                                     <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 185.343 185.343">
                                         <path style="fill:#05bbff;" d="M51.707,185.343c-2.741,0-5.493-1.044-7.593-3.149c-4.194-4.194-4.194-10.981,0-15.175
                                             l74.352-74.347L44.114,18.32c-4.194-4.194-4.194-10.987,0-15.175c4.194-4.194,10.987-4.194,15.18,0l81.934,81.934
