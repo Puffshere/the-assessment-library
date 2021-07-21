206d5bf00cebaa7c5e6019ecf6f516507030b388
Dylan Marsh <dylan@knoware.co>
2021-07-21 08:23:25 -0400

    Trends page wip

---
+++ pages/trends.vue
@@ -0,0 +1,300 @@
+<template>
+    <section class="trends">
+        <LazyHydrate when-visible><main-nav active="hiring"></main-nav></LazyHydrate>
+
+        <section class="header">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <img style="max-width: 400px; width: 100%;" src="~/assets/trends_logo.png" alt="Assessments 24x7 on Trends.co">
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <div class="container" style="margin-bottom: 90px">
+            <div class="row">
+                <div class="col-7">
+                    <h1 class="title">Whitelabel HR Assessment Software</h1>
+                    <h2 class="sub-title">Trusted by brands all over the globe.</h2>
+
+                    <p>
+                        For more than 20 years, Assessments 24x7 has powered business consultants, executive coaches, and Fortune 500s with the tools to 
+                        optimize hiring processes, boost employee productivity, and maximize performance, using our DISC, Motivators and additional 
+                        assessment tools. Our validated assessments, flexible platform, and exceptional customer support have positioned us as a "Global 
+                        Leader in Assessment Technology."
+                    </p><br/>
+                    <a href="#contact" class="button">Try our DISC Assessment For Free</a>
+                    <nuxt-link to="/assessments/disc" class="button secondary">Learn More</nuxt-link>
+                </div>
+
+                <div class="col-5">
+                    <img style="margin:30px auto 0 auto;display:block;width:100%;max-width:400px;box-shadow:0 0 20px rgba(0,0,0,0.1);border-radius:20px" src="~assets/pillar-feature-1.jpg">
+                </div>
+            </div>
+        </div>
+
+        <section class="finding-talent">
+            <div class="container">
+                <div class="row">
+                    <div class="col-5">
+                        <img style="width: 100%; border-radius: 20px; box-shadow: 0 10px 20px rgba(0,0,0,0.1)" src="https://images.unsplash.com/photo-1573497491208-6b1acb260507?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80">
+                    </div>
+
+                    <div class="col-7">
+                        <h3>We Offer the World's Most Advanced Assessment Reseller Dashboard</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut eius vel dolor neque unde ad doloremque quod sed tempore facilis 
+                            ullam recusandae delectus placeat soluta illo, eaque earum ducimus suscipit? Lorem ipsum dolor sit amet, consectetur 
+                            adipisicing elit.
+                        </p><br/>
+
+                        <a href="#contact" class="button outline light">Try our DISC Assessment For Free</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="how-it-works">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <h3>How it Works</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut eius vel dolor neque unde ad doloremque quod sed tempore facilis 
+                            ullam recusandae delectus placeat soluta illo, eaque earum ducimus suscipit?
+                        </p>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-12">
+                        <div class="step-number">1</div>
+                        <div class="step-content">
+                            <h4>Try our DISC Assessment for Free</h4>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut eius vel dolor neque unde ad doloremque quod sed tempore facilis 
+                                ullam recusandae delectus placeat soluta illo, eaque earum ducimus suscipit?
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-12">
+                        <div class="step-number">2</div>
+                        <div class="step-content">
+                            <h4>Phone Consultation</h4>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut eius vel dolor neque unde ad doloremque quod sed tempore facilis 
+                                ullam recusandae delectus placeat soluta illo, eaque earum ducimus suscipit?
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-12">
+                        <div class="step-number">3</div>
+                        <div class="step-content">
+                        <h4>Set Up Your White-Label Assessment Dashboard</h4>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut eius vel dolor neque unde ad doloremque quod sed tempore facilis 
+                                ullam recusandae delectus placeat soluta illo, eaque earum ducimus suscipit?
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-12">
+                        <div class="step-number">4</div>
+                        <div class="step-content">
+                            <h4>Start Selling Assessments</h4>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut eius vel dolor neque unde ad doloremque quod sed tempore facilis 
+                                ullam recusandae delectus placeat soluta illo, eaque earum ducimus suscipit?
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="contact" id="contact">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <h3>Try our DISC Assessment for Free</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut eius vel dolor neque unde ad doloremque quod sed tempore facilis 
+                            ullam recusandae delectus placeat soluta illo, eaque earum ducimus suscipit?
+                        </p>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-12">
+                        <contact-form redirect="https://assessments123.com/?R*oHo%5bv%5b==R" acFormId="18" buttonText="Start DISC Assessment" :isShort="true" />
+                        <br/><br/>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="finding-talent">
+            <div class="container">
+                <div class="row">
+                    <div class="col-4">
+                        <img style="width: 90%; border-radius: 20px; box-shadow: 0 10px 20px rgba(0,0,0,0.1)" src="~/assets/testimonials/susan-cranston.jpg">
+                    </div>
+
+                    <div class="col-8">
+                        <h3>What People Are Saying</h3>
+                        <p>
+                            "One of the best business decisions I made as a certified coach was to explore the solutions offered by Assessments 24x7. 
+                            The online certification courses, forums, training materials and support are world-class. Additionally, the assessment 
+                            reports are personally branded and easy to assess. Working with Assessments 24x7 has supercharged my coaching practice and 
+                            they've become an integral extension of my professional service offering. I highly recommend Assessments 24x7 to any coach, 
+                            consultant, or corporate trainer looking for a one-stop solution that will ensure their business continues to innovate and 
+                            stay forward-thinking."<br/>
+                        </p>
+                        <p>
+                            <strong>Susan Cranston</strong><br/>
+                            Founder & CEO | Authentika Consulting Inc.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <LazyHydrate when-visible><validation></validation></LazyHydrate>
+        <LazyHydrate when-visible><footer-fold></footer-fold></LazyHydrate>
+    </section>
+</template>
+
+<script>
+    import LazyHydrate from 'vue-lazy-hydration';
+
+    export default {
+        components: {
+            LazyHydrate,
+            'main-nav': () => import('@/components/Nav'),
+            'contact-form': () => import('@/components/ContactForm'),
+            'validation': () => import('@/components/ASIValidation'),
+            'footer-fold': () => import('@/components/Footer')
+        },
+        data() {
+            return {
+                structuredDataBreadcrumbs: {
+                    "@context": "http://schema.org",
+                    "@type": "BreadcrumbList",
+                    "itemListElement": [
+                        {
+                            "@type": "ListItem",
+                            "position": "1",
+                            "item": {
+                                "@id": "https://assessments24x7.com/trends",
+                                "name": "Trends for Assessment Software" 
+                            } 
+                        }
+                    ]
+                }
+            }
+        },
+        head() {
+            return {
+                title: 'Trends for Assessment Software | Assessments 24x7',
+                meta: [
+                    {
+                        hid: 'description',
+                        name: 'description',
+                        content: 'Whitelabel HR assessment software by Assessments 24x7. Trusted by brands all over the globe.'
+                    }
+                ],
+                __dangerouslyDisableSanitizers: ['script'],
+                script: [
+                    {
+                        innerHTML: JSON.stringify(this.structuredDataBreadcrumbs),
+                        type: 'application/ld+json'
+                    }
+                ]
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .trends {
+        .header {
+            background: url('~assets/header.jpg');
+            background-size: cover;
+            background-position-y: 70%;
+            color: #fff;
+            text-align: center;
+            padding: 70px 0;
+            margin-bottom: 40px;
+
+            .section-title {
+                font-size: 30pt;
+                margin-top: 0;
+            }
+        }
+
+        .title {
+            font-size: 24pt;
+            margin-bottom: 0;
+            margin-top: 30px;
+        }
+
+        .sub-title {
+            margin-top: 0;
+            font-weight: 400;
+        }
+
+        h3 {
+            font-size: 24pt;
+            margin-bottom: 0;
+            margin-top: 10px;
+        }
+
+        .finding-talent {
+            background: url('~assets/artifacts/content-bg.png');
+            background-size: cover !important;
+            background-position: center;
+            padding: 60px 0;
+            color: #fff;
+        }
+
+        .how-it-works {
+            margin: 50px 0;
+
+            .step-number {
+                background: #0033c5;
+                border-radius: 50%;
+                width: 60px;
+                height: 60px;
+                color: #fff;
+                font-size: 22pt;
+                text-align: center;
+                line-height: 60px;
+                font-weight: 600;
+                position: absolute;
+                margin-top: 20px;
+            }
+
+            .step-content {
+                margin-left: 80px;
+
+                h4 {
+                    font-size: 14pt;
+                    margin-bottom: -10px;
+                }
+            }
+        }
+
+        .screenshots {
+            margin-bottom: 50px;
+        }
+    }
+</style>
