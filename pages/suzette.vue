86b6bd99d41e5a5cd6c062ff0aedc29345105798
Dylan Marsh <dylan@knoware.co>
2021-07-22 10:00:34 -0400

    Adding sales person direct pages

---
+++ pages/suzette.vue
@@ -0,0 +1,101 @@
+<template>
+    <section class="skeleton">
+        <main-nav></main-nav>
+
+        <section class="header">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <h1 class="section-title">Schedule a Meeting</h1>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h2 style="text-align:center">Schedule a Meeting with Suzette Chaparro</h2>
+
+                    <p style="text-align:center">
+                        We’d like the opportunity to discuss your assessment and certification needs in detail. Choose a time slot from the calendar and we’ll get the conversation started!<br/><br/>
+                        
+                        You can also email us at <a class="hyperlink" href="mailto:support@assessments24x7.com">support@assessments24x7.com</a> 
+                        or call us at <a class="hyperlink" href="tel:12064006647">+1 (206) 400-6647</a>
+                    </p><br/>
+
+                    <div class="calendly-inline-widget" :data-url="`https://calendly.com/suzette-247/30min?text_color=000000&primary_color=0033c5`" style="min-width:320px;height:700px;margin-bottom:60px;margin-top:-40px"></div>
+                </div>
+            </div>
+        </div>
+
+        <footer-fold></footer-fold>
+    </section>
+</template>
+
+<script>
+    import axios from 'axios';
+    import Nav from '@/components/Nav';
+    import Footer from '@/components/Footer';
+
+    export default {
+        head() {
+            return {
+                script: [
+                    {
+                        src: 'https://assets.calendly.com/assets/external/widget.js',
+                        async: true
+                    }
+                ]
+            }
+        },
+        components: {
+            'main-nav': Nav,
+            'footer-fold': Footer
+        },
+        mounted() {
+            window.addEventListener('message', this.onCalendlyEvent);
+        },
+        destroyed() {
+            window.removeEventListener('message', this.onCalendlyEvent);
+        },
+        methods: {
+            onCalendlyEvent(e) {
+                if (e.data.event && e.data.event.indexOf('calendly') === 0) {
+                    if (e.data.event === 'calendly.event_scheduled') {
+                        axios.post(`/api/contact/${this.$route.query.contactId}/tag/7`);
+                    }
+                }
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .skeleton {
+        .header {
+            background: url('~assets/about.jpg');
+            background-size: cover;
+            color: #fff;
+            text-align: center;
+            padding: 70px 0;
+            margin-bottom: 30px;
+
+            .section-title {
+                font-size: 30pt;
+                margin-top: 0;
+            }
+        }
+
+        h1 {
+            font-size: 30pt;
+            line-height: 34pt;
+            margin-bottom: 0;
+        }
+
+        h2 {
+            font-size: 20pt;
+            line-height: 24pt;
+        }
+    }
+</style>
