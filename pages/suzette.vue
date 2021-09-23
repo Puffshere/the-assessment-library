a88692b16a8aa86214b8a864d27731c7d65383e9
Dylan Marsh <dylan@knoware.co>
2021-09-23 14:34:29 -0400

    Suzette copy update

--- pages/suzette.vue
+++ pages/suzette.vue
@@ -19,12 +19,12 @@
 
                     <p>
                         <strong>About Me: </strong>Hello, I’m Suzette Chaparro, VP of client success here at Assessments 24x7. My business experience includes working in the client 
-                        support/service, and hospitality business for over 10 years. My primary role with Assessments 24x7 since I joined this company in 
+                        support/service and hospitality business for over 10 years. My primary roles with Assessments 24x7 since I joined this company in 
                         2014 have been client success driven. I am available for all of your assessment, certification, and dashboard questions and 
                         strategies. I primarily work with our clients to ensure 100% satisfaction with their assessments, accounts, training, and more.
                         <br/><br/>
 
-                        <strong>Hobbies & Fun Facts: </strong>When I am not working, you can find me hiking in the back country, cooking new and exciting 
+                        <strong>Hobbies & Fun Facts: </strong>When I am not working, you can find me hiking in the backcountry, cooking new and exciting 
                         meals for loved ones, or hanging out with my two sweet dogs.<br/><br/>
 
                         My DISC Style is: <strong>Di</strong>
