886682e1786544ffa9329d3c6c6c8e91773d260f
Dylan Marsh <dylan@knoware.co>
2021-10-06 10:36:32 -0400

    Removed booking widget from thank you page

--- pages/thank-you.vue
+++ pages/thank-you.vue
@@ -15,16 +15,17 @@
         <div class="container">
             <div class="row">
                 <div class="col-12">
-                    <h2 style="text-align:center">We will respond by email shortly</h2>
+                    <img alt="Message Sent" src="https://cdn.assessments24x7.com/file/assessments24x7-media/illustrations/sent-message.png" width="200" style="margin:20px auto 0 auto;display:block">
+                    
+                    <h2 style="text-align:center">Your information has been submitted</h2>
 
                     <p style="text-align:center">
-                        We’d like the opportunity to discuss your assessment and certification needs in detail. Choose a time slot from the calendar and we’ll get the conversation started!<br/><br/>
-                        
-                        You can also email us at <a class="hyperlink" href="mailto:support@assessments24x7.com">support@assessments24x7.com</a> 
+                        We’d like the opportunity to discuss your assessment and certification needs in detail. You will receive an email with a link to 
+                        choose a time slot on our calendar. You can also email us at <a class="hyperlink" href="mailto:support@assessments24x7.com">support@assessments24x7.com</a> 
                         or call us at <a class="hyperlink" href="tel:12064006647">+1 (206) 400-6647</a>
-                    </p><br/>
+                    </p><br/><br/><br/>
 
-                    <div class="calendly-inline-widget" :data-url="`https://calendly.com/${agent}?text_color=000000&primary_color=0033c5`" style="min-width:320px;height:700px;margin-bottom:60px;margin-top:-40px"></div>
+                    <!--<div class="calendly-inline-widget" :data-url="`https://calendly.com/${agent}?text_color=000000&primary_color=0033c5`" style="min-width:320px;height:700px;margin-bottom:60px;margin-top:-40px"></div>-->
                 </div>
             </div>
         </div>
