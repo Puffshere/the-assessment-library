f1f1033cccf2c2b14fb7bb763390778df97c066f
Dylan Marsh <dylan@knoware.co>
2022-05-09 11:37:34 -0400

    ATD 2022 certification sale page

---
+++ pages/atd2022.vue
@@ -0,0 +1,338 @@
+<template>
+    <section class="bizx">
+        <main-nav/>
+
+        <section class="banner">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <img class="bizx-logo" src="~/assets/atd-alt.png" alt="ATD">
+                        <h2>DISC Certification</h2>
+                        <h3>Sale</h3>
+                        <h4>Online & Self-Paced</h4>
+                    </div>
+                </div>
+            </div>
+
+            <img class="banner-texture" src="~/assets/bizx-texture.png" alt="">
+        </section>
+
+        <section class="promo-body">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <h5>20% OFF</h5>
+                        <h6>Limited time offer<br/>Must be purchased during the conference</h6>
+                    </div>
+                </div>
+
+                <div class="row certifications">
+                    <div class="col-2"></div>
+
+                    <div class="col-4">
+                        <img class="certification-badge" src="~/assets/badges/new/disc-cert-badge-color-label.png" alt="DISC Certification"><br/>
+                        <h6 class="certification-title color-disc">DISC Certification<br/>$1,995 USD</h6>
+                        <a href="https://buy.stripe.com/aEUeVucLz42j4Zq7t3" target="_blank" class="button">Buy DISC Certification</a>
+                    </div>
+
+                    <div class="col-4">
+                        <img class="certification-badge" src="~/assets/badges/new/mot-cert-badge-color-label.png" alt="Motivators Certification"><br/>
+                        <h6 class="certification-title color-mot">Motivators Certification<br/>$1,995 USD</h6>
+                        <a href="https://buy.stripe.com/bIY5kU7rf2Yf0JafZA" target="_blank" class="button mot">Buy Motivators Certification</a>
+                    </div>
+                </div>
+
+                <div class="row" style="margin-top: 75px">
+                    <div class="col-12" style="text-align: center">
+                        <h6 style="margin-bottom: 20px;">Save even more! Purchase both certifications for $3,495</h6>
+                        <a href="https://buy.stripe.com/4gwdRqdPDgP5dvW14H" target="_blank" class="button secondary">Purchase Certification Bundle</a>
+                    </div>
+                </div>
+
+                <!--<div class="row">
+                    <div class="col-12" style="text-align: center;margin-top: 30px;">
+                        <h3 style="color:#e93d2f;font-size:16pt;margin-bottom:0">Download Dr. Tony Alessandra's Presentation Slides:</h3>
+                        <a href="https://cdn.assessments24x7.com/file/assessments24x7-media/downloads/BizX+UK+-+Different+Strokes+for+Different+Folks.pdf" target="_blank" class="hyperlink">Different Strokes for Different Folks</a> &nbsp;|&nbsp;
+                        <a href="https://cdn.assessments24x7.com/file/assessments24x7-media/downloads/BizX+UK+Coaches+-+How+Assessments+Can+Help+You+%26+Your+Clients.pdf" target="_blank"  class="hyperlink">How Assessments Can Help You & Your Clients</a><br/>
+                        <a href="https://cdn.assessments24x7.com/file/assessments24x7-media/downloads/BizX+Las+Vegas+-+Attracting%2C+Training+%26+Retaining+Top+Talent.pdf" target="_blank"  class="hyperlink">Attracting, Training & Retaining Top Talent</a>
+                    </div>
+                </div>-->
+            </div>
+        </section>
+
+        <section class="certification">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <h2>Become A Certified Assessment Practitioner</h2>
+                        <div class="col-6">
+                            <ul>
+                                <li>Improve relationships with colleagues and clients</li>
+                                <li>Gain deeper insights into yourself</li>
+                                <li>Enhance your critical thinking skills</li>
+                                <li>Ongoing, situational support from an Assessments 24x7 Master Certified Practitioner</li>
+                                <li>Receive an assessment & an in-depth, professional debrief by your Master Certified Trainer</li>
+                            </ul>
+                        </div>
+
+                        <div class="col-6">
+                            <ul>
+                                <li>Access Certified Trainer Material to use in your coaching/training business, including a Debrief Guide, PowerPoint presentations, and marketing materials</li>
+                                <li>Get listed on our International Directory of Certified Practitioners linked to your website</li> 
+                                <li>Debrief clients with confidence</li>
+                                <li>Maximize the effectiveness of your assessment debriefs</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-12">
+                        <h2 id="disc">DISC Assessment Certification</h2>
+
+                        <div class="col-3">
+                            <img class="image" src="~/assets/badges/new/disc-cert-badge-color.png" alt="">
+                        </div>
+
+                        <div class="col-9">
+                            <p>
+                                Do you want to significantly improve your understanding of DISC and your ability to debrief DISC reports? Have you ever 
+                                struggled to figure out how to use the assessments for different client needs? Clear up those questions and more with 
+                                Assessments 24x7's DISC Certification program. This is an exceptional investment for every coach, HR specialist, and leadership 
+                                development professional. DISC Certification will give you the knowledge to debrief reports without hesitation- which means 
+                                even more satisfied clients for you!
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="col-12">
+                        <h3 style="margin-bottom: -10px">Course Summary (12 hours):</h3>
+
+                        <div class="col-6">
+                            <ul class="course-summary">
+                                <li class="topic disc">Take the Online DISC Assessment</li>
+                                <li class="topic disc">Independent Review of Select Self-Study Documents</li>
+                                <li class="topic disc">Complete the 10-part DISC Virtual Training Program and all phases of the DISC Communications Coach training</li>
+                            </ul>
+                        </div>
+
+                        <div class="col-6">
+                            <ul class="course-summary">
+                                <li class="topic disc">Receive a personal DISC report debrief from one of our trainers</li>
+                                <li class="topic disc">Review the online Resources for DISC Trainers</li>
+                                <li class="topic disc">DISC Final Knowledge Check and certification call</li>
+                                
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <credits :isLight="true" shrm="12" hrci="10" atd="10" icfCore="5.25" icfRD="12.5" hrciProgramId="582706" />
+
+                <div class="row">
+                    <div class="col-12">
+                        <h2 id="motivators">Motivators Assessment Certification</h2>
+
+                        <div class="col-3">
+                            <img class="image" src="~/assets/badges/new/mot-cert-badge-color.png" alt="">
+                        </div>
+
+                        <div class="col-9">
+                            <p>
+                                The Motivators Assessment Certification Program provides a fresh perspective into human motivation. With this understanding, 
+                                organizations and workplaces can more effectively motivate employees to increase productivity, strengthen collaboration, and 
+                                produce exceptional results. The curriculum teaches the seven Dimensions of Motivation individually so that you learn how they 
+                                affect interactions with others. When you better understand what motivates your workforce – or yourself! – there’s no limit to 
+                                what you can achieve.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="col-12">
+                        <h3 style="margin-bottom: -10px">Course Summary (10 hours):</h3>
+
+                        <div class="col-6">
+                            <ul class="course-summary">
+                                <li class="topic mot">Take the Online Motivators Assessment</li>
+                                <li class="topic mot">Independent Review of Self-Study Reading and Videos</li>
+                                <li class="topic mot">Receive a personal Motivators report debrief from a trainer</li>
+
+                            </ul>
+                        </div>
+
+                        <div class="col-6">
+                            <ul class="course-summary">
+                                <li class="topic mot">Review the online Resources for Motivators Trainers</li>
+                                <li class="topic mot">Motivators Final Knowledge Check</li>
+                                <li class="topic mot">Final Certification Call and discuss strategy for use</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <credits :isLight="true" shrm="10" hrci="7" atd="7" icfCore="7.5" icfRD="9.25" hrciProgramId="582705" />
+            </div>
+        </section>
+
+        <LazyHydrate when-visible><stats style="margin: 80px 0;"></stats></LazyHydrate>
+        <LazyHydrate when-visible><validation style="margin: -30px 0 0;"></validation></LazyHydrate>
+        <LazyHydrate when-visible><footer-fold/></LazyHydrate>
+    </section>
+</template>
+
+<script>
+    import LazyHydrate from 'vue-lazy-hydration';
+
+    export default {
+        components: {
+            'main-nav': () => import('@/components/Nav'),
+            'validation': () => import('@/components/ASIValidation'),
+            'stats': () => import('@/components/Stats'),
+            'credits': () => import('@/components/Credits'),
+            'footer-fold': () => import('@/components/Footer')
+        },
+        data() {
+            return {
+
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .bizx {
+        .banner {
+            background: url('~assets/hero.jpg');
+            background-size: cover;
+            background-position: center;
+            color: #fff;
+            text-align: center;
+            padding: 40px 0 80px;
+            margin-bottom: 30px;
+            position: relative;
+
+            .bizx-logo {
+                width: 100%;
+                max-width: 200px;
+            }
+
+            h2 {
+                font-size: 30pt;
+                margin-top: 20px;
+                margin-bottom: 0;
+            }
+
+            h3 {
+                font-size: 90pt;
+                margin-bottom: 0;
+                margin-top: -20px;
+                text-transform: uppercase;
+                letter-spacing: 2px;
+            }
+
+            h4 {
+                font-size: 26pt;
+                margin-top: -20px;
+            }
+
+            .banner-texture {
+                position: absolute;
+                width: 100%;
+                left: 0;
+                bottom: 0;
+            }
+        }
+
+        .promo-body {
+            margin-bottom: 80px;
+
+            h5 {
+                text-align: center;
+                font-size: 50pt;
+                margin-bottom: 0;
+                margin-top: 0;
+            }
+
+            h6 {
+                text-align: center;
+                font-size: 18pt;
+                margin-top: 0;
+                text-transform: uppercase;
+            }
+
+            .certifications {
+                text-align: center;
+
+                .certification-badge {
+                    width: 100%;
+                    max-width: 200px;
+                    margin-bottom: 20px;
+                }
+
+                .certification-title  {
+                    font-size: 16pt;
+                    margin-top: -10px;
+                    margin-bottom: 20px;
+                }
+
+                .color-disc {
+                    color: #0033c5;
+                }
+
+                .color-mot {
+                    color: #f9690e;
+                }
+
+                .button {
+                    margin-right: 0;
+                }
+            }
+        }
+
+        .certification {
+            .image {
+                height: 220px;
+                margin: 0 auto;
+                display: block;
+            }
+
+            h2 {
+                font-size: 20pt;
+                line-height: 24pt;
+                margin-bottom: 0px;
+            }
+
+            .course-summary {
+                list-style: none;
+                padding-left: 0;
+
+                .topic {
+                    padding: 10px;
+                    background: #f7f7f7;
+                    margin-bottom: 6px;
+                    border-radius: 10px;
+                    color: #fff;
+                    font-weight: 600;
+
+                    &.disc { background: linear-gradient(268deg, #0999fe, #0249ec); }
+                    &.mot { background: linear-gradient(268deg, #ffa518, #fc542a); }
+                    &.hvp { background: linear-gradient(268deg, #8ef03a, #11bd04); }
+                    &.eiq { background: linear-gradient(268deg, #fa4ca6, #ff2f4c); }
+                    &.learning { background: linear-gradient(268deg, #b82cff, #540bfb); }
+                    &.pgi { background: linear-gradient(268deg, #666, #333); }
+                }
+            }
+        }
+
+        h1 {
+            font-size: 30pt;
+            line-height: 34pt;
+            margin-bottom: 0;
+        }
+
+        h2 {
+            font-size: 20pt;
+            line-height: 24pt;
+        }
+    }
+</style>
