7b65ee09e99dbc112385f46fb1161190a05bb1b1
Shawn Taylor <shawncurtistaylor@hotmail.com>
2022-12-15 09:04:00 -0800

    finishes le360chinese report addition

--- pages/sample-reports.vue
+++ pages/sample-reports.vue
@@ -337,8 +337,10 @@
                         <h3>Leadership Effectiveness 360&deg;</h3>
                         <a v-if="lang === 'en-us'" href="https://cdn.assessments24x7.com/file/assessments24x7-media/reports/SampleLE360.pdf" class="hyperlink" target="_blank" rel="noopener">Leadership Effectiveness 360&deg;</a>
                         <a v-else-if="lang === 'vi'" href="https://cdn.assessments24x7.com/file/assessments24x7-media/reports/LeadershipEffectiveness360VietnameseSampleReport.pdf" class="hyperlink" target="_blank" rel="noopener">Leadership Effectiveness 360&deg;</a>
+                        <a v-else-if="lang === 'zh'" href="https://cdn.assessments24x7.com/file/assessments24x7-media/reports/LeadershipEffectiveness360ChineseSampleReport.pdf" class="hyperlink" target="_blank" rel="noopener">Leadership Effectiveness 360&deg;</a>
+
                         <a v-else class="hyperlink disabled">Leadership Effectiveness 360&deg;</a>
-                        <a class="language-button other" @click="le360ReportModal = true">2 languages</a><br/>
+                        <a class="language-button other" @click="le360ReportModal = true">3 languages</a><br/>
                     </div>
                 </div>
 
@@ -889,7 +891,7 @@
                                 <h1>Leadership Effectiveness 360&deg; Reports</h1>
 
                                 <a href="https://cdn.assessments24x7.com/file/assessments24x7-media/reports/SampleLE360.pdf" class="hyperlink" target="_blank" rel="noopener">English</a><br/>
-                                <a href="https://cdn.assessments24x7.com/file/assessments24x7-media/reports/LeadershipEffectiveness360VietnameseSampleReport.pdf" class="hyperlink" target="_blank" rel="noopener">Vietnamese</a>
+                                <a href="https://cdn.assessments24x7.com/file/assessments24x7-media/reports/LeadershipEffectiveness360VietnameseSampleReport.pdf" class="hyperlink" target="_blank" rel="noopener">Vietnamese</a><br/>
                                 <a href="https://cdn.assessments24x7.com/file/assessments24x7-media/reports/LeadershipEffectiveness360ChineseSampleReport.pdf" class="hyperlink" target="_blank" rel="noopener">Chinese</a>
                             </div>
                         </div>
