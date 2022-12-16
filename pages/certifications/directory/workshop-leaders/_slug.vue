3c4ac806ee69ae2badc7c93eb6673f4f5430a9f6
Shawn Taylor <shawncurtistaylor@hotmail.com>
2022-12-16 03:47:03 -0800

    adds ability to add multiple websites

--- pages/certifications/directory/workshop-leaders/_slug.vue
+++ pages/certifications/directory/workshop-leaders/_slug.vue
@@ -10,26 +10,40 @@
                         <h1 class="section-title">{{ leader.name }}</h1>
                         <p>
                             {{ leader.city }}<span v-if="leader.state"> {{ leader.state }}</span>, {{ leader.country }}
-                            
+
                             <span v-if="leader.country === 'United States'">🇺🇸</span>
-                            <span v-else-if="leader.country === 'Canada' || leader.country === 'Nova Scotia'">🇨🇦</span>
+                            <span
+                                v-else-if="leader.country === 'Canada' || leader.country === 'Nova Scotia'">🇨🇦</span>
                             <span v-else-if="leader.country === 'United Kingdom'">🇬🇧</span>
                             <span v-else-if="leader.country === 'Vietnam'">🇻🇳</span>
-                            <span v-else-if="leader.country === 'Australia' || leader.country === 'Western Australia'">🇦🇺</span>
-                            <span v-else-if="leader.country === 'India'">🇮🇳</span><br/>
-                            
+                            <span
+                                v-else-if="leader.country === 'Australia' || leader.country === 'Western Australia'">🇦🇺</span>
+                            <span v-else-if="leader.country === 'India'">🇮🇳</span><br />
+
+                            <!-- <span v-if="leader.website">
+                                <a style="color: #fff" :href="leader.website" target="blank" rel="noopener">
+                                    <b>{{ leader.website.split(",") }}</b>
+                                </a><br/>
+                            </span> -->
+
                             <span v-if="leader.website">
                                 <a style="color: #fff" :href="leader.website" target="blank" rel="noopener">
-                                    <b>{{ leader.website }}</b>
-                                </a><br/>
+                                    <b>
+                                        {{
+                                        leader.website.indexOf(",") === -1
+                                        ? leader.website
+                                        : leader.website.split(",")
+                                        }}
+                                    </b>
+                                </a><br />
                             </span>
 
                             <span v-if="leader.linkedin">
                                 <a style="color: #fff" :href="leader.linkedin" target="blank" rel="noopener">
                                     <b>{{ leader.linkedin }}</b>
-                                </a><br/>
+                                </a><br />
                             </span>
-                            
+
                             {{ leader.email }}
                         </p>
                     </div>
@@ -72,72 +86,72 @@
 </template>
 
 <script>
-    import axios from 'axios';
-    import LazyHydrate from 'vue-lazy-hydration';
+import axios from 'axios';
+import LazyHydrate from 'vue-lazy-hydration';
 
-    export default {
-        components: {
-            LazyHydrate,
-            'main-nav': () => import('@/components/Nav'),
-            'blog-sidebar': () => import('@/components/BlogSidebarLatest'),
-            'footer-fold': () => import('@/components/Footer')
-        },
-        head() {
-            return {
-                title: 'DISC Workshop Leader Bio | Assessments 24x7',
-                meta: [
-                    {
-                        hid: 'description',
-                        name: 'description',
-                        content: 'These experienced trainers have been selected for their abilities to engage, teach, and WOW learners - challenging participants to think differently, and leave our training sessions eager to apply new skills.'
-                    }
-                ]
-            }
-        },
-        data() {
-            return {
-                leader: {}
-            }
-        },
-        async created() {
-            const response = await axios.get(`/api/workshop-leaders/${this.$route.params.slug}`);
-            this.leader = response.data.leader;
+export default {
+    components: {
+        LazyHydrate,
+        'main-nav': () => import('@/components/Nav'),
+        'blog-sidebar': () => import('@/components/BlogSidebarLatest'),
+        'footer-fold': () => import('@/components/Footer')
+    },
+    head() {
+        return {
+            title: 'DISC Workshop Leader Bio | Assessments 24x7',
+            meta: [
+                {
+                    hid: 'description',
+                    name: 'description',
+                    content: 'These experienced trainers have been selected for their abilities to engage, teach, and WOW learners - challenging participants to think differently, and leave our training sessions eager to apply new skills.'
+                }
+            ]
         }
+    },
+    data() {
+        return {
+            leader: {}
+        }
+    },
+    async created() {
+        const response = await axios.get(`/api/workshop-leaders/${this.$route.params.slug}`);
+        this.leader = response.data.leader;
     }
+}
 </script>
 
 <style lang="scss" scoped>
-    .workshop-leader-profile {
-        .header {
-            background: url('~assets/about.jpg');
-            background-size: cover;
-            color: #fff;
-            text-align: center;
-            margin-bottom: 40px;
-            padding: 70px 0;
+.workshop-leader-profile {
+    .header {
+        background: url('~assets/about.jpg');
+        background-size: cover;
+        color: #fff;
+        text-align: center;
+        margin-bottom: 40px;
+        padding: 70px 0;
 
-            .section-title {
-                font-size: 30pt;
-                margin-top: 0;
-            }
-        }
-
-        h2 {
-            margin-bottom: 0;
-            font-size: 20pt;
-            line-height: 24pt;
-        }
-
-        h1 {
+        .section-title {
             font-size: 30pt;
-            line-height: 34pt;
-            margin-bottom: 0;
-        }
-
-        .image {
-            border-radius: 20px;
-            width: 150px;
-            margin-bottom: 20px;
+            margin-top: 0;
         }
     }
+
+    h2 {
+        margin-bottom: 0;
+        font-size: 20pt;
+        line-height: 24pt;
+    }
+
+    h1 {
+        font-size: 30pt;
+        line-height: 34pt;
+        margin-bottom: 0;
+    }
+
+    .image {
+        border-radius: 20px;
+        width: 150px;
+        margin-bottom: 20px;
+    }
+}
 </style>
