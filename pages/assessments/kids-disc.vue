5be3897970fd02734e34f9012222091e0eec4031
Shawn Taylor <shawncurtistaylor@hotmail.com>
2023-01-19 04:23:55 -0800

    updates link to which bird are you store

--- pages/assessments/kids-disc.vue
+++ pages/assessments/kids-disc.vue
@@ -299,7 +299,7 @@
                             styles, themselves, and others.
                         </p><br />
 
-                        <a href to="https://store.assessments24x7.com/products/which-bird-are-you-ten-lesson-curriculum" class="button">Buy The Curriculum Today</a>
+                        <a href="https://store.assessments24x7.com/products/which-bird-are-you-ten-lesson-curriculum" target="_blank" rel="noopener" class="button">Buy The Curriculum Today</a>
                         <nuxt-link to="/assessments" class="button secondary">See Our Other Assessments</nuxt-link>
                     </div>
                 </div>
