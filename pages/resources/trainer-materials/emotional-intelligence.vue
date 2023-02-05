cc001c87f5460be759f27547a2a182f2c004479f
Shawn Taylor <shawncurtistaylor@hotmail.com>
2023-02-05 04:32:24 -0800

    updates emotional intelligence page

--- pages/resources/trainer-materials/emotional-intelligence.vue
+++ pages/resources/trainer-materials/emotional-intelligence.vue
@@ -42,7 +42,7 @@
                             <small>Overview and Guide to debrief the assessment report</small></td></tr>
                             <tr v-else><td><a class="hyperlink" href="https://cdn.assessments24x7.com/file/assessments24x7-media/trainer-materials/eiq/19+EIQ+Debrief+Guide+_NC_vs4FINAL.pdf" target="_blank" rel="noopener">Emotional Intelligence Debrief Guide</a>
                             <small>Overview and Guide to debrief the assessment report</small></td></tr>
-                            <tr v-if="certified"><td><a class="hyperlink" href="https://cdn.assessments24x7.com/file/assessments24x7-media/trainer-materials/eiq/Overview%20of%20EIQ-graphic.pdf" target="_blank" rel="noopener">EIQ Graphic</a>
+                            <tr><td><a class="hyperlink" href="https://f002.backblazeb2.com/file/assessments24x7-media/trainer-materials/eiq/02_EIQ+Graphic+2023.pdf" target="_blank" rel="noopener">EIQ Graphic</a>
                             <small>Overview of the EIQ model in a single page graphic outlining priorities and impact</small></td></tr>
                             <tr v-if="certified"><td><a class="hyperlink" href="https://cdn.assessments24x7.com/file/assessments24x7-media/trainer-materials/eiq/EIQ_DISCIntegration.pdf" target="_blank" rel="noopener">EI and DISC Expressions</a>
                             <small>One-page document showing DISC styles and corresponding expression for high and low EIQ</small></td></tr>
