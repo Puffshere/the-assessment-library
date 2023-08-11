0bec19e31c5386dd3a77dc91da8060f0e55abcaa
Shawn Taylor <shawncurtistaylor@hotmail.com>
2023-08-11 03:23:43 -0700

    creates new DISC-workshop-essentials page

---
+++ pages/certifications/DISC-workshop-essentials.vue
@@ -0,0 +1,359 @@
+<template>
+    <section class="skeleton">
+        <LazyHydrate when-visible><main-nav active="training"></main-nav></LazyHydrate>
+
+        <section class="header">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <h1 class="section-title">DISC Workshop Leader Essentials</h1>
+                        <p>
+                            The quest to ensure our clients are the most knowledgeable and best-equipped in the business
+                            continues with our innovative DISC Workshop Leader Essentials! Developed by assessment industry
+                            leaders and public speaking professionals, DISC Workshop Leader Essentials modules equip you
+                            with the tools needed to lead engaging, immersive workshops.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h2>DISC Workshop Leader Essentials</h2>
+                    <p>
+                        These comprehensive, focused training modules include all the content, materials, and supporting
+                        resources to deliver workshops to a variety of audiences, including internal teams, small or large
+                        businesses, customer service groups, and educational institutions. Perfect for individuals and teams
+                        to take their personal and professional development to the next level, these workshops are designed
+                        to intentionally broaden and strengthen behavioral awareness and communication skills quickly and
+                        efficiently.
+                    </p><br />
+
+                    <nuxt-link to="/about/why-use-assessments" class="button outline">Why Use Assessments?</nuxt-link>
+                    <nuxt-link to="/contact" class="button">Contact a Specialtist</nuxt-link><br /><br />
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-12">
+                    <h3 style="margin-bottom: -10px">Included With Each DISC Workshop Module</h3>
+
+                    <p>
+                        The DISC workshop modules provide everything an instructor needs to deliver a variety of training
+                        workshops, including marketing materials, PowerPoint presentations, leader guides, participant
+                        guides, and activities. Additionally, a training video with instructions on how to use all materials
+                        is included with each module. The five DISC workshop modules are:
+                    </p>
+
+                    <ul>
+                        <li>Self-Awareness & Communication</li>
+                        <li>Transitioning from Peer to Leader</li>
+                        <li>Trust Based Leadership</li>
+                        <li>Making Teams Work</li>
+                        <li>Sales Optimization</li>
+                    </ul>
+
+                    <div class="col-6">
+                        <ul class="course-summary">
+                            <li class="topic disc">Watch the orientation video</li>
+                            <li class="topic disc">Submit bio and headshot</li>
+                            <li class="topic disc">Review the workshop training video</li>
+                        </ul>
+                    </div>
+
+                    <div class="col-6">
+                        <ul class="course-summary">
+                            <li class="topic disc">Study training materials & other resources</li>
+                            <li class="topic disc">Market your workshop</li>
+                            <li class="topic disc">Prepare using the included Planning Checklist</li>
+                        </ul>
+                    </div>
+                </div>
+            </div><br /><br />
+
+            <div class="row">
+                <div class="col-3">
+                    <img class="image" src="~/assets/workshop-certification-images/communication.jpeg"
+                        alt="Self-awareness and Communication">
+                </div>
+
+                <div class="col-9">
+                    <h3>Self-awareness and Communication</h3>
+
+                    <p>
+                        This course helps workshop participants gain a deeper understanding of themselves and how they
+                        interact with others. It reveals each participant's unique behavioral style and provides practical
+                        guidance on how to use this knowledge to improve communication and relationships.
+                        Most interpersonal conflict is caused by misunderstanding and communication breakdowns. By
+                        understanding their own behavioral style and the styles of others, anyone can learn to communicate
+                        more effectively and build stronger relationships. This can lead to reduced conflict, increased
+                        productivity, and improved performance in both professional and personal relationships.
+                    </p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-3">
+                    <img class="image" src="~/assets/workshop-certification-images/peer-to-leader.jpeg"
+                        alt="Transitioning from Peer to Leader">
+                </div>
+
+                <div class="col-9">
+                    <h3>Transitioning from Peer to Leader</h3>
+
+                    <p>
+                        Transitioning from Peer to Leader prepares new leaders for the many challenges of their new role.
+                        Participants will learn how to effectively communicate with employees, manage performance
+                        expectations, and develop action plans. The course is centered around understanding the different
+                        roles and responsibilities of successful contributors as they make the transition to leadership.
+                        Participants will learn how to use DISC communication skills to navigate employee stressors and
+                        build strong relationships. They will also learn how to set clear expectations and provide feedback
+                        in a way that motivates employees. Moving from peer to leader can be a significant change, but this
+                        course can help new leaders make a smoother transition by gaining the critical skills and knowledge
+                        they need to take on their new role.
+                    </p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-3">
+                    <img class="image" src="~/assets/workshop-certification-images/trust.jpeg" alt="Trust-Based Leadership">
+                </div>
+
+                <div class="col-9">
+                    <h3>Trust-based Leadership</h3>
+
+                    <p>
+                        Trust is the foundation of all strong relationships. The most critical relationships in an
+                        organization are those between leaders and individual contributors. DISC Workshop Leader Essentials:
+                        Trust-based Leadership can help organizations create an environment of leadership influence rooted
+                        in trust by teaching effective communication, credibility, and confidence. Participants will grow to
+                        understand the impact of their leadership style on others, communicate effectively with different
+                        behavioral styles, build credibility and earn trust, create an environment of accountability and
+                        responsibility, and identify individual and team needs. By focusing on the leadership roles and
+                        responsibilities that create trusting relationships, organizations can ensure a productive work
+                        environment focused on growth and retention.
+                    </p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-3">
+                    <img class="image" src="~/assets/workshop-certification-images/make-teams-work.jpeg"
+                        alt="Making Teams Work">
+                </div>
+
+                <div class="col-9">
+                    <h3>Making Teams Work</h3>
+
+                    <p>
+                        Teamwork is the combined action of a group of people, especially when it is effective and efficient.
+                        This DISC workshop can help organizations develop the skills, strategies, and tools they need to
+                        create teams that excel. During this team-focused skill-building course, participants will learn to
+                        assemble and interact with teams based on complementary behaviors as well as embracing and
+                        understanding diversity and its benefits. This will help to improve communication, cooperation, and
+                        results-based outcomes. Participants will also learn to distinguish varying beliefs and behaviors
+                        using the insightful DISC Team Dynamics report.
+                    </p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-3">
+                    <img class="image" src="~/assets/workshop-certification-images/sales.jpeg" alt="Sales Optimization">
+                </div>
+
+                <div class="col-9">
+                    <h3>Sales Optimization</h3>
+
+                    <p>
+                        DISC is a powerful tool that can be used to improve sales performance by understanding the
+                        behavioral styles of others and adapting your selling strategy to identify and meet the needs of
+                        your prospects. This can lead to more closed deals and stronger, longer relationships with clients.
+                        In addition to identifying behavioral, communication, and buying styles, this course will also teach
+                        participants to build and maintain relationships with clients, transform buyers into brand
+                        advocates, identify client needs, manage expectations, and complete a thorough exploration of a
+                        sales process from A to Z. This course is ideal for sales professionals of all levels who want to
+                        improve their sales skills and build customer retention.
+                    </p><br />
+                </div>
+            </div>
+
+            <div class="row cert-div">
+                <div class="col-3 cert">
+                    <img class="image-DISC-Cert" src="/images/DISC-Cert-Emblem.png" alt="DISC Certification">
+                </div>
+
+                <div class="col-9">
+                    <div class="disc-cert-text">DISC Certification is required prior to purchasing any DISC Workshop Leader
+                        Essentials package.</div>
+                </div><br />
+            </div>
+            <br>
+            <br>
+            <!-- <credits :isLight="true" shrm="15" hrci="11.5" atd="11.5" icfCore="6" icfRD="9" hrciProgramId="582708" /> -->
+        </div>
+        <div class="row">
+            <div class="col-12 cert-credits">
+                <h3>* Continuing Education credits are not available for DISC Workshop Leader Essentials programs.</h3>
+            </div>
+        </div>
+        <br>
+        <LazyHydrate when-visible><kevin-mccarthy></kevin-mccarthy></LazyHydrate>
+        <LazyHydrate when-visible><footer-fold></footer-fold></LazyHydrate>
+    </section>
+</template>
+
+<script>
+import LazyHydrate from 'vue-lazy-hydration';
+
+export default {
+    components: {
+        LazyHydrate,
+        'main-nav': () => import('@/components/Nav'),
+        'kevin-mccarthy': () => import('@/components/testimonials/KevinMcCarthy'),
+        'footer-fold': () => import('@/components/Footer'),
+        'credits': () => import('@/components/Credits')
+    },
+    data() {
+        return {
+            structuredDataBreadcrumbs: {
+                "@context": "http://schema.org",
+                "@type": "BreadcrumbList",
+                "itemListElement": [
+                    {
+                        "@type": "ListItem",
+                        "position": "1",
+                        "item": {
+                            "@id": "https://assessments24x7.com/certifications",
+                            "name": "Certifications"
+                        }
+                    },
+                    {
+                        "@type": "ListItem",
+                        "position": "2",
+                        "item": {
+                            "@id": "https://assessments24x7.com/certifications/workshop-certification",
+                            "name": "Workshop Leader Certification"
+                        }
+                    }
+                ]
+            }
+        }
+    },
+    head() {
+        return {
+            title: 'DISC Workshop Essentials: Your All-in-one Workshop Solutions',
+            meta: [
+                {
+                    hid: 'description',
+                    name: 'description',
+                    content: 'Discover innovative out-of-the-box workshop solutions with DISC Workshop Leader Essentials. Teach engaging workshops with our comprehensive training modules, which include all the all the content, materials, and resources needed to deliver workshops to your audience. Elevate personal and professional growth, lead with confidence, and optimize sales.'
+                }
+            ],
+            __dangerouslyDisableSanitizers: ['script'],
+            script: [
+                {
+                    innerHTML: JSON.stringify(this.structuredDataBreadcrumbs),
+                    type: 'application/ld+json'
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.skeleton {
+    .header {
+        background: url('~assets/about.jpg');
+        background-size: cover;
+        color: #fff;
+        text-align: center;
+        padding: 70px 0;
+        margin-bottom: 30px;
+
+        .section-title {
+            font-size: 30pt;
+            margin-top: 0;
+        }
+    }
+
+    h1 {
+        font-size: 30pt;
+        line-height: 34pt;
+        margin-bottom: 0;
+    }
+
+    h2 {
+        font-size: 20pt;
+        line-height: 24pt;
+    }
+
+    h3 {
+        margin-top: 0;
+        margin-bottom: 0;
+    }
+
+    .image {
+        width: 100%;
+        border-radius: 20px;
+    }
+
+    .cert {
+        text-align: center;
+    }
+    .cert-credits {
+        text-align: center;
+    }
+
+    .cert-div {
+        display: flex;
+        align-items: center;
+    }
+
+    .image-DISC-Cert {
+        width: 50%;
+        border-radius: 20px;
+        margin-left: 20px;
+    }
+
+    .disc-cert-text {
+        font-size: 23px;
+        font-weight: 700;
+    }
+
+    .course-summary {
+        list-style: none;
+        padding-left: 0;
+
+        .topic {
+            padding: 10px;
+            background: #f7f7f7;
+            margin-bottom: 6px;
+            border-radius: 10px;
+            color: #fff;
+            font-weight: 600;
+
+            &.disc {
+                background: linear-gradient(268deg, #0999fe, #0249ec);
+            }
+        }
+    }
+    @media (max-width: 600px) {
+        .image-DISC-Cert {
+            width: 70%;
+            border-radius: 20px;
+        }
+    
+        .disc-cert-text {
+            font-size: large;
+            font-weight: 700;
+        }
+    
+    }
+}
+
+</style>
