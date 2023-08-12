c82ed981c6040cad91ebde6cd2a1654d3895ccf0
Shawn Taylor <shawncurtistaylor@hotmail.com>
2023-08-12 05:26:46 -0700

    adds finished new relationship strategies page

--- pages/new-relationship-strategies.vue
+++ pages/new-relationship-strategies.vue
@@ -10,10 +10,6 @@
                 </div>
             </div>
         </section>
-
-
-
-
         <section class="slides">
             <div class="container">
                 <div class="row slides-container">
@@ -27,36 +23,54 @@
                             <a target="_blank"
                                 href="https://f002.backblazeb2.com/file/assessments24x7-media/platinum-rule/1+A+Proven+Way+to+Build+Rapport+%26+Understanding+the+Four+Basic+Styles.mp3">
                                 <li class="mp3Links mp3Buttons"><span class="numbers">1.</span>
+                                    <span>
+                                        <img class="imageMp3" src="images/beeping.png">
+                                    </span>
                                     A Proven Way to Build Rapport & Understanding the Four Basic Styles
                                 </li>
                             </a>
                             <a target="_blank"
                                 href="https://f002.backblazeb2.com/file/assessments24x7-media/platinum-rule/2+Getting+a+Quick+Handle+on+Anyone+%26+The+Platinum+Grid.mp3">
                                 <li class="mp3Links mp3Buttons"><span class="numbers">2.</span>
+                                    <span>
+                                        <img class="imageMp3" src="images/beeping.png">
+                                    </span>
                                     Getting a Quick Handle on Anyone & The Platinum Grid
                                 </li>
                             </a>
                             <a target="_blank"
                                 href="https://f002.backblazeb2.com/file/assessments24x7-media/platinum-rule/3+Pinpointing+Your+Blend+of+Personal+Styles+%26+Building+the+Bridges+of+Compatibility.mp3">
                                 <li class="mp3Links mp3Buttons"><span class="numbers">3.</span>
+                                    <span>
+                                        <img class="imageMp3" src="images/beeping.png">
+                                    </span>
                                     Pinpointing Your Blend of Personal Styles & Building the Bridges of Compatibility
                                 </li>
                             </a>
                             <a target="_blank"
                                 href="https://f002.backblazeb2.com/file/assessments24x7-media/platinum-rule/4+Mastering+the+Skill+of+Adaptability+%26+Bringing+Out+the+Best+in+Groups.mp3">
                                 <li class="mp3Links mp3Buttons"><span class="numbers">4.</span>
+                                    <span>
+                                        <img class="imageMp3" src="images/beeping.png">
+                                    </span>
                                     Mastering the Skill of Adaptability & Bringing Out the Best in Groups
                                 </li>
                             </a>
                             <a target="_blank"
                                 href="https://f002.backblazeb2.com/file/assessments24x7-media/platinum-rule/5+High-Performance+Leadership+%26+Five+Steps+to+Successful+Sales.mp3">
                                 <li class="mp3Links mp3Buttons"><span class="numbers">5.</span>
+                                    <span>
+                                        <img class="imageMp3" src="images/beeping.png">
+                                    </span>
                                     High-Performance Leadership & Five Steps to Successful Sales
                                 </li>
                             </a>
                             <a target="_blank"
                                 href="https://f002.backblazeb2.com/file/assessments24x7-media/platinum-rule/6+Five+Steps+to+Successful+Sales+(cont%E2%80%99d)+%26+Creating+a+Web+of+Rich+Relationships.mp3">
                                 <li class="mp3Links mp3Buttons"><span class="numbers">6.</span>
+                                    <span>
+                                        <img class="imageMp3" src="images/beeping.png">
+                                    </span>
                                     Five Steps to Successful Sales (cont’d) & Creating a Web of Rich Relationships
                                 </li>
                             </a>
@@ -172,6 +186,12 @@ export default {
 
         .numbers {
             float: left;
+        }
+
+        .imageMp3 {
+            width: 3%;
+            float: left;
+            margin-left: 20px;
         }
 
         .slideLink {
