a473c73f18db3876ecf120da1ca1db3940ebab64
Shawn Taylor <shawncurtistaylor@hotmail.com>
2023-11-02 13:39:45 -0700

    push testing

--- pages/tools/email.vue
+++ pages/tools/email.vue
@@ -202,7 +202,7 @@
                     <div class="row col-12 button-container">
 
                         <div class="col-4">
-                            <button @click="translateText" class="btn translateBtn">translate</button>
+                            <button @click="translateText" class="btn translateBtn">Translate</button>
                         </div>
 
                         <div class="col-4 center-button">
