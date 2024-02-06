17086220001c3bf2557e56de050a8d83792ac928
Shawn Taylor <shawncurtistaylor@hotmail.com>
2024-02-06 09:47:02 -0800

    working on slack

--- api/index.js
+++ api/index.js
@@ -141,10 +141,8 @@ app.post('/slack/events', (req, res) => {
 
     // If there's no challenge, it's an actual event
     console.log('Handling Slack event...');
-    res.status(200).send('Event received');
+    res.status(200).send('Event received', req);
 });
-
-  
 
 app.post('/completions', async (req, res) => {
     try {
