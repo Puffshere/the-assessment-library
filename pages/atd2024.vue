c8402bdb0dd729a739e2a32d342ab0cca6245f35
Shawn Taylor <shawncurtistaylor@hotmail.com>
2024-03-11 06:26:45 -0700

    adds atd page

---
+++ pages/atd2024.vue
@@ -0,0 +1,668 @@
+<template>
+  <section class="bizx">
+    <main-nav />
+    <section class="banner1">
+      <div class="container">
+        <br />
+        <div class="row">
+          <div class="col-12">
+            <div class="col-6">
+              <img class="bizx-logo" src="~/assets/ATD_New_Orleans_Logo.png" alt="ATD" />
+              <br />
+              <img class="blocks" src="~/assets/DISC_Blocks_top_image.png" alt="blocks" />
+            </div>
+            <br />
+            <br />
+            <br />
+            <br />
+            <div class="col-6">
+              <h3>DISC</h3>
+              <h3>BLOCKS</h3>
+              <h4>A Practical Tool for Enhanced</h4>
+              <h4>Collaboration and Understanding</h4>
+              <br />
+              <br />
+              <button class="orderBtn">ORDER NOW</button>
+              <br />
+              <br />
+              <img class="whiteArrow" src="~/assets/White_Arrow_Pointing.png" alt="white arrow" />
+              <br />
+              <img class="findUs" src="~/assets/Find_us_at_ATD_Navy_blue_background.png" alt="find us logo" />
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12"></div>
+        </div>
+      </div>
+      <img class="bannerBottomWave" src="~/assets/Wave_1.png" alt="bottom wave" />
+    </section>
+    <br />
+    <section class="whiteSection1">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <h1>DISC BLOCKS SERVE AS...</h1>
+            <br />
+            <p>
+              Effective tools for style awareness, team building, stress relief,
+              and communication enhancement. With their vibrant colors and
+              versatility, DISC Blocks offer a practical and engaging way to
+              enhance collaboration and understanding within groups and teams.
+            </p>
+            <p>
+              These blocks are four, soft Lego®-like pieces, each representing
+              one of the DISC styles and a corresponding Adaptability phrase to
+              immediately set the tone for interaction:
+            </p>
+            <br />
+            <div class="row">
+              <div class="col-3">
+                <img src="~/assets/Red_D_Dominance.png" alt="red block" class="block" />
+              </div>
+              <div class="col-3">
+                <img src="~/assets/Yellow_I_Influence.png" alt="yellow block" class="block" />
+              </div>
+              <div class="col-3">
+                <img src="~/assets/Green_S_Steadiness.png" alt="green block" class="block" />
+              </div>
+              <div class="col-3">
+                <img src="~/assets/Blue_C_Conscientiousness.png" alt="blue block" class="block" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <section class="banner2">
+      <img src="~/assets/Wave_2.png" alt="top wave" class="bannerTopWave" />
+      <div class="container">
+        <br />
+        <div class="row">
+          <div class="col-12">
+            <br />
+            <br />
+            <div class="col-5">
+              <h3>Why</h3>
+              <h3>DISC BLOCKS?</h3>
+              <br />
+              <P>
+                By integrating DISC Blocks into the workplace, you can create a
+                dynamic, engaging environment that not only highlights
+                individual communication preferences but also promotes a deeper
+                understanding of how these styles contribute to the collective
+                success of the team's efforts.
+              </P>
+              <br />
+              <br />
+              <br />
+              <button class="orderBtn">ORDER NOW</button>
+            </div>
+            <div class="col-7">
+              <br />
+              <img class="a-f-l-m" src="~/assets/asian_female_leading_meeting.png" alt="female leading meeting" />
+              <br />
+              <br />
+              <img class="whiteArrow" src="~/assets/White_Arrow_Pointing.png" alt="white arrow" />
+              <br />
+              <img class="findUs" src="~/assets/Find_us_at_ATD_Navy_blue_background.png" alt="find us logo" />
+            </div>
+          </div>
+        </div>
+        <br />
+      </div>
+      <img class="bannerBottomWave" src="~/assets/Wave_1.png" alt="bottom wave" />
+    </section>
+    <br />
+    <section class="whiteSection2">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <h1>BENEFITS</h1>
+            <br />
+            <div class="row">
+              <div class="col-9">
+                <img src="~/assets/people_raising_hands.png" alt="people raising hands" class="peopleRaisingHands" />
+              </div>
+              <div class="col-3">
+                <h2>
+                  Improved
+                  <br>
+                  Team Dynamics
+                  <br>
+                  <hr>
+                </h2>
+                <p>
+                  By visualizing and acknowledging distinct communication
+                  styles, teams can better understand and leverage the diversity
+                  of perspectives, leading to improved collaboration and
+                  productivity.
+                </p>
+              </div>
+            </div>
+            <br />
+            <div class="row">
+              <div class="col-3">
+                <h2>
+                  Tailored
+                  <br>
+                  Approach
+                  <br>
+                  <hr>
+                </h2>
+                <p>
+                  DISC Blocks enable team members to adapt their communication
+                  strategies to suit different situations and audiences within
+                  the organization, allowing for more tailored and successful
+                  interactions.
+                </p>
+              </div>
+              <div class="col-3">
+                <h2>
+                  Enhanced
+                  <br>
+                  Self-Awareness
+                  <br>
+                  <hr>
+                </h2>
+                <p>
+                  DISC Blocks encourage individuals to reflect on their
+                  communication styles, fostering self-awareness and personal
+                  growth, which can lead to more effective interactions and
+                  leadership within the organization.
+                </p>
+              </div>
+              <div class="col-3">
+                <h2>
+                  Continuous
+                  <br>
+                  Learning
+                  <br>
+                  <hr>
+                </h2>
+                <p>
+                  Implementing DISC Blocks promotes an environment of continuous
+                  learning and development, as team members regularly assess and
+                  discuss communication styles, leading to ongoing improvements
+                  in team efficacy.
+                </p>
+              </div>
+              <div class="col-3">
+                <h2>
+                  Conflict
+                  <br>
+                  Reduction
+                  <br>
+                  <hr>
+                </h2>
+                <p>
+                  The use of DISC Blocks can help preempt misunderstandings and
+                  conflicts by providing a clear framework for interpreting team
+                  members' behaviors and responses.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <section class="banner3">
+      <img src="~/assets/Wave_2.png" alt="top wave" class="bannerTopWave" />
+      <div class="container">
+        <br />
+        <div class="row">
+          <div class="col-12">
+            <br />
+            <br />
+            <div class="col-5">
+              <br />
+              <div class="blocks">
+                <img src="~/assets/DISC_Blocks_Single_Stack_Image.png" alt="disc blocks single stack" />
+              </div>
+            </div>
+            <div class="col-7">
+              <h3>INTRODUCING THE</h3>
+              <h3>DISC BLOCKS</h3>
+              <br />
+              <p>
+                Give each member a complete set of DISC Blocks during or after review of the DISC assessment. Show them
+                how each block represents a primary style and can be used either individually to showcase one style in
+                action or as a group to reveal a blend of styles present at one time. Introduce the adaptability phrase
+                on each block as a quick way to connect with the needs of that style.
+                Then, consider initiating a 'Block of the Week' activity or communication - each week a different DISC
+                Block is highlighted to help educate the team about that style’s traits and encourage team members to
+                explore and appreciate diverse working and communication styles.
+              </p>
+              <br />
+              <br />
+              <br />
+              <button class="orderBtn">ORDER NOW</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <div class="title">
+              <h3>
+                VISUALLY COMMUNICATING YOUR
+              </h3>
+              <h3>
+                STYLE TO OTHERS
+              </h3>
+            </div>
+            <br />
+            <div class="row">
+              <div class="col-12">
+                <div class="col-5">
+                  <p>
+                    One way to utilize these blocks is by using them as a means of communicating your current style to
+                    team members. For instance, if you are in a direct and task-oriented mode, you can place the Red
+                    Block on top to indicate this style. This way, team members will understand the need for brevity,
+                    directness, and getting straight to the point, or simply exchanging greetings.
+                  </p>
+                  <br />
+                  <br />
+                  <p>
+                    In addition to indicating current communication preferences, DISC Blocks can be used in a variety of
+                    ways to keep the principles of DISC prevalent in the daily workflow. For instance, during team
+                    meetings, individuals might place their block of choice in front of them, acting as a non-verbal cue
+                    to others on how they are approaching the meeting. Another option could be to incorporate the blocks
+                    into desk organization; employees could have a small pedestal where they display the block
+                    representing their dominant style for the day or project. This method helps foster an environment
+                    where team members can adjust their interactions based on visible cues.
+                  </p>
+                  <br />
+                  <br />
+                  <br />
+                  <button class="orderBtn">ORDER NOW</button>
+                </div>
+                <div class="col-7">
+                  <br />
+                  <div class="blocks">
+                    <img src="~/assets/male_business_man.png" alt="male business man" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <br />
+            <br />
+            <br />
+            <br />
+          </div>
+        </div>
+      </div>
+      <img class="bannerBottomWave" src="~/assets/Wave_1.png" alt="bottom wave" />
+    </section>
+    <br />
+    <br />
+    <br />
+    <br />
+    <section class="whiteSection3">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <h1>APPLICATION</h1>
+            <h2>
+              Building Communication & Understanding as a Team/Group
+            </h2>
+            <br />
+            <div class="row">
+              <div class="col-12 peopleSmiling">
+                <img src="~/assets/team_smiling.png" alt="team smiling" />
+              </div>
+            </div>
+            <br />
+            <div class="row">
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <br />
+    <br />
+    <br />
+    <br />
+    <div class="findUsFooter">
+      <img src="~/assets/ATD_Find_us_at.png" alt="find us at white background" />
+    </div>
+    <br />
+    <br />
+    <br />
+    <br />
+    <LazyHydrate when-visible><footer-fold /></LazyHydrate>
+  </section>
+</template>
+
+
+<script>
+export default {
+  components: {
+    "main-nav": () => import("@/components/Nav"),
+    "footer-fold": () => import("@/components/Footer"),
+  },
+};
+</script>
+
+
+<style lang="scss" scoped>
+.bizx {
+  .banner1 {
+    background-color: #1e222d;
+    color: #fff;
+    text-align: center;
+    margin-bottom: 60px;
+    position: relative;
+
+    h1 {
+      font-size: 34pt;
+      line-height: 34pt;
+      margin-bottom: 0;
+    }
+
+    h2 {
+      font-size: 30pt;
+      margin-top: 20px;
+      margin-bottom: 0;
+    }
+
+    h3 {
+      font-size: 60pt;
+      margin-bottom: -35px;
+      margin-top: 0;
+      margin-left: 120px;
+      text-transform: uppercase;
+      letter-spacing: 2px;
+      text-align: left;
+    }
+
+    h4 {
+      margin-left: 120px;
+      font-size: 15pt;
+      font-weight: 300;
+      margin-bottom: -30px;
+      text-align: left;
+    }
+
+    .bizx-logo {
+      width: 100%;
+      max-width: 220px;
+      left: 40px;
+      position: absolute;
+    }
+
+    .blocks {
+      margin-top: 110px;
+      margin-left: 80px;
+    }
+
+    .orderBtn {
+      margin-left: -120px;
+    }
+
+    .findUs {
+      max-width: 280px;
+      margin-left: 120px;
+      margin-top: -30px;
+    }
+
+    .bannerBottomWave {
+      position: absolute;
+      width: 100%;
+      left: 0;
+      bottom: -170px;
+      z-index: -10;
+    }
+
+    .bannerTopWave {
+      position: absolute;
+      width: 100%;
+      left: 0;
+      top: -180px;
+      z-index: -10;
+    }
+  }
+
+  .whiteSection1 {
+    h1 {
+      font-size: 34pt;
+      line-height: 34pt;
+      margin-bottom: 0;
+    }
+  }
+
+  .banner2 {
+    background-color: #1e222d;
+    color: #fff;
+    text-align: center;
+    margin-bottom: 60px;
+    position: relative;
+
+    h1 {}
+
+    h2 {}
+
+    h3 {
+      font-size: 40pt;
+      margin-bottom: -35px;
+      margin-top: 0;
+      margin-left: 0px;
+      text-transform: uppercase;
+      letter-spacing: 2px;
+      text-align: left;
+    }
+
+    h4 {
+      font-size: 15pt;
+      font-weight: 300;
+      margin-bottom: -30px;
+      text-align: left;
+    }
+
+    P {
+      font-weight: 300;
+      margin-bottom: -30px;
+      text-align: left;
+    }
+
+    .a-f-l-m {
+      width: 100%;
+    }
+
+    .orderBtn {
+      float: left;
+    }
+
+    .findUs {
+      max-width: 380px;
+      margin-left: 220px;
+      margin-top: -80px;
+    }
+
+    .bannerBottomWave {
+      position: absolute;
+      width: 100%;
+      left: 0;
+      bottom: -170px;
+      z-index: -10;
+    }
+
+    .bannerTopWave {
+      position: absolute;
+      width: 100%;
+      left: 0;
+      top: -180px;
+      z-index: -10;
+    }
+  }
+
+  .whiteSection2 {
+    h1 {
+      font-size: 36pt;
+      line-height: 34pt;
+      margin-bottom: 0;
+      margin-left: 20px;
+    }
+
+    .peopleRaisingHands {
+      width: 100%;
+      margin-left: 0px;
+    }
+
+    hr {
+      height: 6px;
+      background-color: #1e222d;
+      width: 67px;
+      float: left;
+      margin-top: -5px;
+    }
+  }
+
+  .banner3 {
+    background-color: #1e222d;
+    color: #fff;
+    text-align: center;
+    margin-bottom: 60px;
+    position: relative;
+
+    h3 {
+      font-size: 40pt;
+      margin-bottom: -35px;
+      margin-top: 0;
+      margin-left: 0px;
+      text-transform: uppercase;
+      letter-spacing: 2px;
+      text-align: left;
+    }
+
+    h4 {
+      font-size: 15pt;
+      font-weight: 300;
+      margin-bottom: -30px;
+      text-align: left;
+    }
+
+    P {
+      font-weight: 300;
+      margin-bottom: -30px;
+      text-align: left;
+    }
+
+    .title {
+      margin-left: 40px;
+    }
+
+    .blocks {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      img {
+        max-width: 100%;
+      }
+    }
+
+    .orderBtn {
+      float: left;
+    }
+
+    .findUs {
+      max-width: 320px;
+      margin-left: 190px;
+      margin-top: -80px;
+    }
+
+    .bannerBottomWave {
+      position: absolute;
+      width: 100%;
+      left: 0;
+      bottom: -170px;
+      z-index: -10;
+    }
+
+    .bannerTopWave {
+      position: absolute;
+      width: 100%;
+      left: 0;
+      top: -180px;
+      z-index: -10;
+    }
+  }
+
+  .whiteSection3 {
+    h1 {
+      font-size: 36pt;
+      line-height: 34pt;
+      margin-bottom: 0;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+
+    h2 {
+      font-size: 20pt;
+      font-weight: 400;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin-top: 10px;
+    }
+
+    .peopleSmiling {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      img {
+        max-width: 100%;
+      }
+    }
+  }
+
+  .block {
+    width: 200px;
+    border-radius: 20px;
+  }
+
+  .orderBtn {
+    background-color: #095edd;
+    color: white;
+    border-radius: 10px;
+    height: 50px;
+    width: 140px;
+  }
+
+  .whiteArrow {
+    max-width: 100px;
+    margin-left: -300px;
+  }
+
+  .findUsFooter {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    img {
+      max-width: 100%;
+    }
+  }
+}
+</style>
