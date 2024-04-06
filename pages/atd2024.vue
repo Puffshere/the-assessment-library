37d542b807fde79bffa489a6c6e6fd50e546d76c
Shawn Taylor <shawncurtistaylor@hotmail.com>
2024-04-06 04:33:10 -0700

    adds brinna updates to atd2024

--- pages/atd2024.vue
+++ pages/atd2024.vue
@@ -57,13 +57,13 @@
             <br />
             <div class="row">
               <div class="col-3">
-                <img src="~/assets/Dominance.png" alt="red block" class="block" />
+                <!-- <img src="~/assets/Dominance.png" alt="red block" class="block" /> -->
               </div>
               <div class="col-3">
-                <img src="~/assets/Influence.png" alt="yellow block" class="block" />
+                <!-- <img src="~/assets/Influence.png" alt="yellow block" class="block" /> -->
               </div>
               <div class="col-3">
-                <img src="~/assets/Steadiness.png" alt="green block" class="block" />
+                <!-- <img src="~/assets/Steadiness.png" alt="green block" class="block" /> -->
               </div>
               <div class="col-3">
                 <!-- <img src="~/assets/Conscientiousness.png" alt="blue block" class="block" /> -->
