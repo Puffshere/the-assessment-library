b69a880b5487e820eeef762da4a15991b78d4eb0
Shawn Taylor <shawncurtistaylor@hotmail.com>
2024-04-08 05:26:10 -0700

    trying to fix multiple + issue in front of phone number

--- plugins/aos.js
+++ plugins/aos.js
@@ -1,7 +1,7 @@
-// plugins/aos.js
-import AOS from "aos";
-import "aos/dist/aos.css";
+// // plugins/aos.js
+// import AOS from "aos";
+// import "aos/dist/aos.css";
 
-export default ({ app }) => {
-    app.AOS = new AOS.init({ disable: "phone" });
-};
+// export default ({ app }) => {
+//     app.AOS = new AOS.init({ disable: "phone" });
+// };
