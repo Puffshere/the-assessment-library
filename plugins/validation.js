8b9557d914787fe1835d0e3c344b55b87f0c926d
Shawn Taylor <shawncurtistaylor@hotmail.com>
2024-04-10 08:11:28 -0700

    trying to fix multiple + issue in front of phone number

--- plugins/validation.js
+++ plugins/validation.js
@@ -1,10 +1,10 @@
-import { extend } from 'vee-validate';
-import * as rules from 'vee-validate/dist/rules';
-import { messages } from 'vee-validate/dist/locale/en.json';
+// import { extend } from 'vee-validate';
+// import * as rules from 'vee-validate/dist/rules';
+// import { messages } from 'vee-validate/dist/locale/en.json';
 
-Object.keys(rules).forEach(rule => {
-    extend(rule, {
-        ...rules[rule],
-        message: messages[rule]
-    });
-});
+// Object.keys(rules).forEach(rule => {
+//     extend(rule, {
+//         ...rules[rule],
+//         message: messages[rule]
+//     });
+// });
