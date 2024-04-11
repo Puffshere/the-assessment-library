7e75e13571ca71969f83af83f7fad3e542eb5dfb
Shawn Taylor <shawncurtistaylor@hotmail.com>
2024-04-11 15:10:00 -0700

    get in touch page improvements

--- pages/get-in-touch.vue
+++ pages/get-in-touch.vue
@@ -76,26 +76,82 @@
                     <div class="row flex-container">
                         <div class="col-12">
                             <div class="col-6">
-                                <img src="~/assets/api.png" alt="api photo" class="thumbnail" />
+                                <img src="~/assets/api.png" alt="api photo" class="" />
+                                <img src="~/assets/api-icon.png" alt="api icon" class="icon" />
+                                <h3>API Intergration</h3>
+                                <hr />
+                                <p>
+                                    <span class="paragraph">Unlock the full potential of your existing systems</span>
+                                    with seamless API integration.
+                                    Our technical customization perspective enables us to integrate our assessments into
+                                    your company's processes, whether it's a learning management system, intranet,
+                                    employee tracking system, or internal HR systems. Utilize our assessment data and
+                                    reports effortlessly by tapping into our API, a level of customization that many of
+                                    our competitors overlook. Streamline your workflows and enhance efficiency with our
+                                    API integration solutions.
+                                </p>
                             </div>
                             <div class="col-6">
-                                <img src="~/assets/custom-reports.png" alt="custom reports photo" class="thumbnail" />
+                                <img src="~/assets/custom-reports.png" alt="custom reports photo" class="" />
+                                <img src="~/assets/custom-reports-icon.png" alt="custom reports icon" class="icon" />
+                                <h3>Custom Reports</h3>
+                                <hr />
+                                <p>
+                                    <span class="paragraph">Stand out from the crowd with our customizable</span>
+                                    reports that
+                                    cater to your specific requirements. Whether you need to tailor text,
+                                    incorporate static content, adjust colors, or include graphics, we've got you
+                                    covered. Our reports offer a variety of angles, providing insights that are
+                                    relevant and actionable. With our easy-to-use customization tools, creating
+                                    personalized reports that resonate with your audience has never been easier.
+                                    Elevate your reporting capabilities and make informed decisions with our
+                                    comprehensive customization options.
+                                </p>
                             </div>
                         </div>
                         <div class="row flex-container">
                             <div class="col-12">
                                 <div class="col-6">
                                     <img src="~/assets/branding-and-user-experience.png"
-                                        alt="branding and user experience photo" class="thumbnail" />
+                                        alt="branding and user experience photo" class="" />
+                                    <img src="~/assets/branding-and-user-experience-icon.png"
+                                        alt="branding and user experience icon" class="icon" />
+                                    <h3>Branding and User Experience</h3>
+                                    <hr />
+                                    <p>
+                                        <span class="paragraph">Make a lasting impression with branded dashboards</span>
+                                        and personalized user
+                                        experiences. Enhance the look and feel of your platform by incorporating your
+                                        company's colors and logos. Customize the user interface to match your brand
+                                        identity and create a cohesive experience. Whether it's modifying text or
+                                        adjusting the overall appearance, we empower you to make it exactly what you
+                                        want. By focusing on customization, we ensure that every interaction reflects
+                                        your brand's values and fosters meaningful engagement with your audience.
+                                    </p>
                                 </div>
                                 <div class="col-6">
                                     <img src="~/assets/domain-customization.png" alt="domain customization photo"
-                                        class="thumbnail" />
+                                        class="" />
+                                    <img src="~/assets/domain-customization-icon.png" alt="domain customization icon"
+                                        class="icon" />
+                                    <h3>Domain Specific Customization</h3>
+                                    <hr />
+                                    <p>
+                                        <span class="paragraph">Elevate your brand presence and create a seamless</span>
+                                        experience for your employees
+                                        and clients with domain-specific customization. Our technology allows you to
+                                        private label assessments, reports, and dashboards, ensuring that they reflect
+                                        your business identity, including using your own domain name. From custom
+                                        branding to personalized interfaces, we empower you to appear as the direct
+                                        provider of these assessments, strengthening your brand recognition and
+                                        enhancing trust among your audience. Unlock the power of domain-specific
+                                        customization to elevate your organization to new heights of professionalism and
+                                        authenticity.
+                                    </p>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <br />
                     <br />
                     <br />
                 </div>
@@ -211,9 +267,26 @@ export default {
             font-weight: 400;
         }
 
+        h3 {
+            font-size: 28px;
+            margin-bottom: 10px;
+        }
+
+        hr {
+            margin: 0px 50px;
+        }
+
+        .paragraph {
+            font-weight: 700;
+        }
+
+        p {
+            padding: 0 22px;
+        }
+
         .col-6 {
             background-color: white;
-            min-height: 640px;
+            min-height: 720px;
             border-radius: 10px;
             margin-bottom: 60px;
         }
@@ -222,6 +295,11 @@ export default {
             max-width: 100%;
             border-radius: 20px;
             padding: 5px;
+        }
+
+        .icon {
+            width: 22%;
+            margin-top: -70px;
         }
     }
 }
