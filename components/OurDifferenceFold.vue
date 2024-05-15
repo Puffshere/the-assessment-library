0f1546637b5de7b44a9cad0c69c7051612c2d386
Shawn Taylor <shawncurtistaylor@hotmail.com>
2024-05-15 02:44:17 -0700

    makes our difference fold mobile responsive and fixes moving button bug on disc page

--- components/OurDifferenceFold.vue
+++ components/OurDifferenceFold.vue
@@ -8,35 +8,22 @@
         <div class="container">
             <div class="row">
                 <div class="col-12">
-                    <h1>
-                        Our
-                    </h1>
-                    <h1>
+                    <h1>Our
+                        <br />
                         Difference
                     </h1>
-                    <br />
-                    <br />
-                    <br />
-                    <br />
-                    <br />
-                    <br />
                     <h3>
                         Discover our comprehensive suite of <span style="color: #00a8ff; font-weight: 600">DISC
                             assessments</span>, offering the flexibility to align with
-                        YOUR
-                        organization's mission, vision, and core values. Seamlessly integrate our technology <span
-                            style="color: #00a8ff; font-weight: 600">Into your
-                            current
-                            system via our robust API service</span>, empowering you to infuse your organization's
-                        culture into
-                        customized
+                        YOUR organization's mission, vision, and core values. Seamlessly integrate our technology <span
+                            style="color: #00a8ff; font-weight: 600">Into your current system via our robust API
+                            service</span>, empowering you to infuse your organization's culture into customized
                         reports.
                     </h3>
                     <h3>
                         Manage assessments through your personalized dashboard and utilize our extensive training
-                        materials
-                        library, which includes coaching resources, workshop facilitator materials, and a 52-week email
-                        series.
+                        materials library, which includes coaching resources, workshop facilitator materials, and a
+                        52-week email series.
                     </h3>
                     <h3>
                         Take your knowledge to the next level with DISC certification to maximize the benefits of our
@@ -45,13 +32,16 @@
                         ensuring
                         prompt and thorough answers to your questions.
                     </h3>
-                    <a href="#form"><img src="~/assets/disc/get-started-button.png" alt="submit icon"
-                            class="submitIcon" /></a>
+                    <div class="button-container">
+                        <a href="#form"><img src="~/assets/disc/get-started-button.png" alt="submit icon"
+                                class="submitIcon" /></a>
+                    </div>
                 </div>
             </div>
         </div>
     </section>
 </template>
+
 
 
 <style lang="scss" scoped>
@@ -67,14 +57,6 @@
         font-weight: bold;
     }
 
-    h2 {
-        color: white;
-        font-size: 45px;
-        margin-top: -5px;
-        margin-bottom: -10px;
-        font-weight: 400;
-    }
-
     h3 {
         font-size: 28px;
         color: #00a8ff;
@@ -84,15 +66,16 @@
 h1 {
     font-size: 48px;
     padding: 20px;
-    margin-bottom: -150px;
-    margin-left: 10px;
+    line-height: 74px;
+    margin: 0 0;
+    margin-top: -50px;
+    margin-bottom: 40px;
 }
-
 
 .ourDifference {
     position: relative;
     width: 100%;
-    height: 100vh;
+    min-height: 100vh;
     overflow: hidden;
 }
 
@@ -110,15 +93,27 @@ h1 {
     position: relative;
     z-index: 1;
     width: 100%;
-    height: 100%;
+    min-height: 100vh;
     display: flex;
-    margin-top: -20px;
+    flex-direction: column;
+    justify-content: center;
+    box-sizing: border-box;
+}
+
+.ourDifference .row {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 100%;
+}
+
+.ourDifference .button-container {
+    display: flex;
+    justify-content: flex-end;
 }
 
 .ourDifference .submitIcon {
-    position: absolute;
-    right: 20px;
-    bottom: 95px;
+    margin-top: 80px;
     width: 220px;
     height: auto;
 }
@@ -137,9 +132,38 @@ h1 {
     padding: 0 46px;
 }
 
-@media (max-width: 1100px) {
+@media (max-width: 768px) {
 
-    .ourDifference {
+
+    .ourDifference .container h1 {
+        font-size: 48px;
+        margin: 0;
+        line-height: 50px;
+    }
+
+    .ourDifference .row {
+        flex-direction: column;
+        justify-content: flex-start;
+        margin-top: 50px;
+    }
+
+    .ourDifference .container {
+        padding: 20px;
+    }
+
+    .ourDifference .button-container {
+        justify-content: flex-end;
+    }
+
+    .ourDifference .submitIcon {
+        margin-top: 20px;
+        margin-bottom: 40px;
+        width: 180px;
+    }
+
+    .ourDifference .container h3 {
+        font-size: 20px;
+        padding: 0 0px;
     }
 }
 </style>
