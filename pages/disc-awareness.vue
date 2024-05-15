c052537e4c78ae4e15e9017c36131b4db96d8900
Shawn Taylor <shawncurtistaylor@hotmail.com>
2024-05-14 19:41:11 -0700

    disc awareness contact form fold

---
+++ pages/disc-awareness.vue
@@ -0,0 +1,35 @@
+<template>
+    <section>
+      <main-nav class="nav"></main-nav>
+      <section class="body">
+        <contact-form></contact-form>
+      </section>
+      <footer-fold class="foot"></footer-fold>
+    </section>
+  </template>
+  
+  <script>
+  import Nav from '@/components/Nav';
+  import ContactForm from '@/components/DiscAwarenessContactFormFold';
+  import Footer from '@/components/Footer';
+  
+  export default {
+    components: {
+      'main-nav': Nav,
+      'contact-form': ContactForm,
+      'footer-fold': Footer
+    },
+    head() {
+      return {
+        title: 'DISC Awareness | Assessments 24x7',
+        meta: [
+          {
+            hid: 'description',
+            name: 'description',
+            content: ''
+          }
+        ]
+      }
+    }
+  }
+  </script>
