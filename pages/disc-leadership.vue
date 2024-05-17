478cd3731575d1a7c720970ae3e7195700b84f46
Shawn Taylor <shawncurtistaylor@hotmail.com>
2024-05-17 09:41:48 -0700

    adds hero fold to disc leadership page

---
+++ pages/disc-leadership.vue
@@ -0,0 +1,53 @@
+<template>
+    <section>
+      <main-nav class="nav"></main-nav>
+      <section class="body">
+        <disc-leadership-hero></disc-leadership-hero>
+        <enhancing></enhancing>
+        <strengths></strengths>
+        <examples></examples>
+        <awareness-concepts></awareness-concepts>
+        <contact-form></contact-form>
+        <asi></asi>
+      </section>
+      <footer-fold class="foot"></footer-fold>
+    </section>
+  </template>
+  
+  <script>
+  import Nav from '@/components/Nav';
+  import DiscLeadershipHero from '@/components/DiscLeadershipHeroFold';
+  import Enhancing from '@/components/EnhancingFold';
+  import Strengths from '@/components/StrengthsFold';
+  import Examples from '@/components/ExamplesFold';
+  import AwarenessConcepts from '@/components/AwarenessConceptsFold';
+  import ContactForm from '@/components/DiscAwarenessContactFormFold';
+  import Asi from '@/components/ASIFold';
+  import Footer from '@/components/Footer';
+  
+  export default {
+    components: {
+      'main-nav': Nav,
+      'disc-leadership-hero': DiscLeadershipHero,
+      'enhancing': Enhancing,
+      'strengths': Strengths,
+      'examples': Examples,
+      'awareness-concepts': AwarenessConcepts,
+      'contact-form': ContactForm,
+      'asi': Asi,
+      'footer-fold': Footer
+    },
+    head() {
+      return {
+        title: 'DISC Awareness | Assessments 24x7',
+        meta: [
+          {
+            hid: 'description',
+            name: 'description',
+            content: ''
+          }
+        ]
+      }
+    }
+  }
+  </script>
