809f3b7ae5b1177223e789a9c1cc1cabcce9871b
Shawn Taylor <shawncurtistaylor@hotmail.com>
2024-05-18 04:31:54 -0700

    adds intergrating fold to disc leadership page

--- pages/disc-leadership.vue
+++ pages/disc-leadership.vue
@@ -1,53 +1,53 @@
 <template>
-    <section>
-      <main-nav class="nav"></main-nav>
-      <section class="body">
-        <disc-leadership-hero></disc-leadership-hero>
-        <enhancing></enhancing>
-        <strengths></strengths>
-        <examples></examples>
-        <intergrating></intergrating>
-        <contact-form></contact-form>
-        <asi></asi>
-      </section>
-      <footer-fold class="foot"></footer-fold>
+  <section>
+    <main-nav class="nav"></main-nav>
+    <section class="body">
+      <disc-leadership-hero></disc-leadership-hero>
+      <enhancing></enhancing>
+      <strengths></strengths>
+      <examples></examples>
+      <intergrating></intergrating>
+      <contact-form></contact-form>
+      <asi></asi>
     </section>
-  </template>
-  
-  <script>
-  import Nav from '@/components/Nav';
-  import DiscLeadershipHero from '@/components/DiscLeadershipHeroFold';
-  import Enhancing from '@/components/EnhancingFold';
-  import Strengths from '@/components/StrengthsFold';
-  import Examples from '@/components/ExamplesFold';
-  import Intergrating from '@/components/IntergratingFold';
-  import ContactForm from '@/components/DiscAwarenessContactFormFold';
-  import Asi from '@/components/ASIFold';
-  import Footer from '@/components/Footer';
-  
-  export default {
-    components: {
-      'main-nav': Nav,
-      'disc-leadership-hero': DiscLeadershipHero,
-      'enhancing': Enhancing,
-      'strengths': Strengths,
-      'examples': Examples,
-      'intergrating': Intergrating,
-      'contact-form': ContactForm,
-      'asi': Asi,
-      'footer-fold': Footer
-    },
-    head() {
-      return {
-        title: 'DISC Awareness | Assessments 24x7',
-        meta: [
-          {
-            hid: 'description',
-            name: 'description',
-            content: ''
-          }
-        ]
-      }
+    <footer-fold class="foot"></footer-fold>
+  </section>
+</template>
+
+<script>
+import Nav from '@/components/Nav';
+import DiscLeadershipHero from '@/components/DiscLeadershipHeroFold';
+import Enhancing from '@/components/EnhancingFold';
+import Strengths from '@/components/StrengthsFold';
+import Examples from '@/components/ExamplesFold';
+import Intergrating from '@/components/IntergratingFold';
+import ContactForm from '@/components/DiscAwarenessContactFormFold';
+import Asi from '@/components/ASIFold';
+import Footer from '@/components/Footer';
+
+export default {
+  components: {
+    'main-nav': Nav,
+    'disc-leadership-hero': DiscLeadershipHero,
+    'enhancing': Enhancing,
+    'strengths': Strengths,
+    'examples': Examples,
+    'intergrating': Intergrating,
+    'contact-form': ContactForm,
+    'asi': Asi,
+    'footer-fold': Footer
+  },
+  head() {
+    return {
+      title: 'DISC Awareness | Assessments 24x7',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: ''
+        }
+      ]
     }
   }
-  </script>
+}
+</script>
