ca7b8dcb5a983aa3ff728dacf5623f36045803d4
Shawn Taylor <shawncurtistaylor@hotmail.com>
2024-05-20 16:36:35 -0700

    changes interity fold boxes to fix width of the full screen on disc leadership page

--- components/InsightsFold.vue
+++ components/InsightsFold.vue
@@ -2,37 +2,39 @@
     <section class="insights" id="insights">
         <div class="container">
             <div class="row">
-                <div class="col-12 flex-container">
-                    <div class="boxes" style="background-color: #0453f1">
-                        <h3>Leadership Insights
+                <div class="full-width">
+                    <div class="flex-container">
+                        <div class="boxes" style="background-color: #0453f1">
+                            <h3>Leadership Insights
+                                <br />
+                                Tailored to You
+                            </h3>
+                            <img src="~/assets/disc-leadership/left-icon.png" alt="d circle" class="icon" />
                             <br />
-                            Tailored to You
-                        </h3>
-                        <img src="~/assets/disc-leadership/left-icon.png" alt="d circle" class="icon" />
-                        <br />
-                        <p>Unlock personalized leadership strategies based on the unique DISC style to maximize impact
-                            and effectiveness in leading teams.</p>
-                    </div>
-                    <div class="boxes" style="background-color: #1e222d;">
-                        <h3>Enhance Team
+                            <p>Unlock personalized leadership strategies based on the unique DISC style to maximize impact
+                                and effectiveness in leading teams.</p>
+                        </div>
+                        <div class="boxes" style="background-color: #1e222d;">
+                            <h3>Enhance Team
+                                <br />
+                                Collaboration
+                            </h3>
+                            <img src="~/assets/disc-leadership/middle-icon.png" alt="i circle" class="icon" />
                             <br />
-                            Collaboration
-                        </h3>
-                        <img src="~/assets/disc-leadership/middle-icon.png" alt="i circle" class="icon" />
-                        <br />
-                        <p>Learn customized communication techniques to connect authentically with diverse styles,
-                            fostering a more harmonious and productive team environment.</p>
-                    </div>
-                    <div class="boxes" style="background-color: #00a8ff;">
-                        <h3>Drive Organizational
+                            <p>Learn customized communication techniques to connect authentically with diverse styles,
+                                fostering a more harmonious and productive team environment.</p>
+                        </div>
+                        <div class="boxes" style="background-color: #00a8ff;">
+                            <h3>Drive Organizational
+                                <br />
+                                Success
+                            </h3>
+                            <img src="~/assets/disc-leadership/right-icon.png" alt="s circle" class="icon" />
                             <br />
-                            Success
-                        </h3>
-                        <img src="~/assets/disc-leadership/right-icon.png" alt="s circle" class="icon" />
-                        <br />
-                        <p>Elevate performance & organizational success by leveraging tailored leadership strategies to
-                            enhance communication, motivation, & collaboration based on individual DISC styles.
-                        </p>
+                            <p>Elevate performance & organizational success by leveraging tailored leadership strategies to
+                                enhance communication, motivation, & collaboration based on individual DISC styles.
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -47,26 +49,35 @@
 .insights {
     background-color: #1e222d27;
 
-    .flex-container {
-        display: flex;
-        justify-content: space-between;
-        flex-wrap: wrap;
-        margin-top: 0px;
-        margin-bottom: -75px;
-    }
+    .full-width {
+        width: 100vw;
+        position: relative;
+        left: 50%;
+        right: 50%;
+        margin-left: -50vw;
+        margin-right: -50vw;
 
+        .flex-container {
+            display: flex;
+            justify-content: space-between;
+            flex-wrap: wrap;
+            margin-top: 0px;
+            margin-bottom: -75px;
+            width: 100%;
+        }
+    }
+    
     .boxes {
-        flex: 1 1 calc(33%);
+        flex: 1 1 calc(33.3333%);
         text-align: center;
         padding: 20px 15px;
         color: white;
         align-items: center;
 
         .icon {
-            width: 26%;
+            width: 15%;
             display: block;
             margin: 0 auto;
-            margin-top: 10px;
             margin-bottom: -10px;
         }
 
@@ -78,6 +89,7 @@
         p {
             margin-top: 0px;
             line-height: 22px;
+            padding: 0px clamp(0px, 30%, 135px);
         }
     }
 }
@@ -94,6 +106,10 @@
         .boxes {
             flex: 1 1 100%;
             margin-bottom: 20px;
+
+            .icons {
+                width: 28%;
+            }
         }
     }
 }
