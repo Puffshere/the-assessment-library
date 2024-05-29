89ab54ef002deb9aea896bd37911a7563e539136
Shawn Taylor <shawncurtistaylor@hotmail.com>
2024-05-28 20:30:13 -0700

    adds alternating lead calendar

--- pages/alternating-calendar.vue
+++ pages/alternating-calendar.vue
@@ -1,17 +1,14 @@
 <template>
-  <div>Hello</div>
+  <div></div>
 </template>
 
 
 <script>
 export default {
   async asyncData({ $axios, redirect }) {
-    console.log("this is the first console log");
     try {
-      const response = await $axios.get('/api/getCalendarPage');
-      console.log("this is the response", response);
+      const response = await $axios.get('http://localhost:3000/api/getCalendarPage');
       const page = response.data.page;
-      console.log("this is the page", page);
       redirect(`/${page}`);
     } catch (error) {
       console.error('Error fetching calendar page:', error);
--- server/db.js
+++ server/db.js
@@ -1,30 +1,26 @@
-
 // server/db.js
-const { MongoClient } = require('mongodb');
-const url = "mongodb://website:3pUpfMCc0E28ghdRLYOLFkbm7oCocvGg2zbSR6FegubepttZar5nfsx6WCvOx4SMkAeQ1DVwp7lfH8onG6diVQ==@assessments24x7.mongo.cosmos.azure.com:10255/website?ssl=true&sslverifycertificate=false";
-const dbName = 'website';
-let client;
+const mongoose = require('mongoose');
 
-async function connectToDatabase() {
-  if (!client) {
-    client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
-    await client.connect();
-  }
-  return client.db(dbName);
-}
+const uri = "mongodb://website:3pUpfMCc0E28ghdRLYOLFkbm7oCocvGg2zbSR6FegubepttZar5nfsx6WCvOx4SMkAeQ1DVwp7lfH8onG6diVQ==@assessments24x7.mongo.cosmos.azure.com:10255/website?ssl=true&sslverifycertificate=false";
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const visitCounterSchema = new mongoose.Schema({
+    _id: String,
+    count: Number
+}, { collection: 'visitCounter' });
+
+const VisitCounter = mongoose.model('VisitCounter', visitCounterSchema);
 
 async function incrementAndGetPage() {
-  const db = await connectToDatabase();
-  const collection = db.collection('visitCounter');
+    const result = await VisitCounter.findOneAndUpdate(
+        { _id: 'counter' },
+        { $inc: { count: 1 } },
+        { new: true, upsert: true }
+    );
 
-  const result = await collection.findOneAndUpdate(
-    { _id: 'counter' },
-    { $inc: { count: 1 } },
-    { returnOriginal: false, upsert: true }
-  );
-
-  const count = result.value.count;
-  return count % 2 === 0 ? 'monica' : 'angie-w';
+    const count = result.count;
+    return count % 2 === 0 ? 'monica' : 'angie-w';
 }
 
 module.exports = { incrementAndGetPage };
