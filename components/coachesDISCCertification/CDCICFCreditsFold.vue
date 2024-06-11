c6449c99f8af727c5ac9cb4ce80173de938ad17c
Shawn Taylor <shawncurtistaylor@hotmail.com>
2024-06-11 12:19:23 -0700

    adds hrci and atd icons to coaches disc certification page

--- components/coachesDISCCertification/CDCICFCreditsFold.vue
+++ components/coachesDISCCertification/CDCICFCreditsFold.vue
@@ -4,24 +4,28 @@
             <div class="row">
                 <div class="col-12">
                     <div class="col-3">
-                        <img src="~/assets/coaches-disc-certification/icf-logo.png" alt="image">
+                        <img class="icon1" src="~/assets/coaches-disc-certification/icf-logo.png" alt="image">
                         <p><span class="topLine">8.25/6.75</span> <br />
-                            <span class="bottomLine">Core/RD ICF</span></p>
+                            <span class="bottomLine">Core/RD ICF</span>
+                        </p>
                     </div>
                     <div class="col-3">
-                        <img src="~/assets/coaches-disc-certification/icf-logo.png" alt="image">
+                        <img class="icon2" src="~/assets/coaches-disc-certification/icf-logo.png" alt="image">
                         <p><span class="topLine">12 Credits</span> <br />
-                            <span class="bottomLine">SHRM**</span></p>
+                            <span class="bottomLine">SHRM**</span>
+                        </p>
                     </div>
                     <div class="col-3">
-                        <img src="~/assets/coaches-disc-certification/icf-logo.png" alt="image">
+                        <img class="icon3" src="~/assets/coaches-disc-certification/hrci-logo.png" alt="image">
                         <p><span class="topLine">7.5 Credits</span> <br />
-                            <span class="bottomLine">HRCI*</span></p>
+                            <span class="bottomLine">HRCI*</span>
+                        </p>
                     </div>
                     <div class="col-3">
-                        <img src="~/assets/coaches-disc-certification/icf-logo.png" alt="image">
+                        <img class="icon4" src="~/assets/coaches-disc-certification/atd-logo.png" alt="image">
                         <p><span class="topLine">7.5 Credits</span> <br />
-                            <span class="bottomLine">ATD</span></p>
+                            <span class="bottomLine">ATD</span>
+                        </p>
                     </div>
                 </div>
             </div>
@@ -35,28 +39,42 @@
 
 <style lang="scss" scoped>
 @import './CDC.scss';
+
 .container {
     padding-top: 40px;
     padding-bottom: 30px;
 }
 
 .topLine {
-font-size: 28px;
-font-weight: 700;
+    font-size: 30px;
+    font-weight: 700;
 }
 
 .bottomLine {
-font-size: 22px;
-font-weight: 400;
+    font-size: 24px;
+    font-weight: 400;
 }
 
 .col-3 {
     text-align: center;
     color: white;
 
-    img {
-        width: 75%;
+    .icon1 {
+        width: 85%;
         padding-bottom: 20px;
+    }
+
+    .icon2 {
+        width: 85%;
+        padding-bottom: 20px;
+    }
+
+    .icon3 {
+        width: 45%;
+    }
+
+    .icon4 {
+        width: 45%;
     }
 }
 
