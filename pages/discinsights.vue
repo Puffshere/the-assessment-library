0213d9ecd9aeb18ba707031843ae80b81668c74a
Shawn Taylor <shawncurtistaylor@hotmail.com>
2024-06-18 21:02:37 -0700

    adds disc insights page

---
+++ pages/discinsights.vue
@@ -0,0 +1,35 @@
+<template>
+    <section>
+      <main-nav class="nav"></main-nav>
+      <section class="body">
+       
+      </section>
+      <footer-fold class="foot"></footer-fold>
+    </section>
+  </template>
+  
+  <script>
+  import Nav from '@/components/Nav';
+
+  import Footer from '@/components/Footer';
+  
+  export default {
+    components: {
+      'main-nav': Nav,
+      
+      'footer-fold': Footer
+    },
+    head() {
+      return {
+        title: 'DISC Insights | Assessments 24x7',
+        meta: [
+          {
+            hid: 'description',
+            name: 'description',
+            content: ''
+          }
+        ]
+      }
+    }
+  }
+  </script>
