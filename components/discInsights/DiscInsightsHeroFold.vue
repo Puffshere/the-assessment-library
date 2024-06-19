b36e15878f4543c456b4da1e84d3accde1b0e6c6
Shawn Taylor <shawncurtistaylor@hotmail.com>
2024-06-19 03:15:45 -0700

    adds hero fold to disc insights page

---
+++ components/discInsights/DiscInsightsHeroFold.vue
@@ -0,0 +1,152 @@
+<template>
+    <section>
+        <div class="body" style="padding: 0 270px;">
+            <div class="row">
+                <div class="col-12">
+                    <div class="col-6" style="padding-left: 50px;">
+                        <h2
+                            style="line-height: 59px; font-size: 48px; font-weight: 700; color: #1e222d; text-align: center; text-shadow: #00000053 0px 1.8px 7.64998px;">
+                            Hire Smart <br />
+                            with <span style="color: #00a8ff;">DISC Insights</span>
+                        </h2>
+                        <p
+                            style="font-weight: 400; color: #1e222d; font-size: 21px; line-height: 29px; text-align: justify; padding: 0 40px; padding-bottom: 60px;">
+                            Empower your team with actionable insights from our DISC Workplace Insights and Executive
+                            Insights Reports.
+                        </p>
+                    </div>
+                    <div class="col-6" style="position: relative;">
+                        <img src="~/assets/disc-insights/hero-main-graphic.png" alt="hero main graphic image"
+                            style="position: absolute; z-index: 5; right: 20px;" />
+                    </div>
+                    <div class="col-12" style="position: relative;">
+                        <img src="~/assets/disc-insights/hero-black-graphic.png" alt="hero black graphic image"
+                            style="width: 100%; z-index: 1; max-height: 205px;" />
+                        <div style="margin-top: -125px;">
+                            <button class="button" :disabled="step === 3"
+                                :style="{ backgroundColor: '#00a8ff', marginLeft: '50px', border: 'none', fontSize: '16px', minHeight: '40px', width: '170px', opacity: step === 3 ? 0.5 : 1 }">
+                                DISC <span style="text-decoration: underline;">Workplace</span> Insights
+                                <span>
+                                    <img src="~/assets/disc-insights/right-arrow.png"
+                                        style="width: 35px; float: right; margin-top: -3px; margin-right: -16px;"
+                                        alt="right arrow icon">
+                                </span>
+                            </button>
+                            <button class="button" :disabled="step === 3"
+                                :style="{ backgroundColor: '#00a8ff', border: 'none', fontSize: '16px', minHeight: '40px', width: '170px', opacity: step === 3 ? 0.5 : 1 }">
+                                DISC <span style="text-decoration: underline;">Executive</span> Insights
+                                <span>
+                                    <img src="~/assets/disc-insights/right-arrow.png"
+                                        style="width: 35px; float: right; margin-top: -3px; margin-right: -16px;"
+                                        alt="right arrow icon">
+                                </span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    methods: {
+        scrollToSection(event) {
+            const element = document.getElementById('icfSection');
+            if (element) {
+                element.scrollIntoView({ behavior: 'smooth' });
+            }
+            event.target.blur();
+        },
+        scrollToSectionButton(event) {
+            const element = document.getElementById('cdcContactForm');
+            if (element) {
+                element.scrollIntoView({ behavior: 'smooth' });
+            }
+            event.target.blur();
+        },
+        // handleIntersection(entries) {
+        //     entries.forEach(entry => {
+        //         if (entry.isIntersecting) {
+        //             entry.target.classList.add('animate-rise');
+        //         } else {
+        //             entry.target.classList.remove('animate-rise');
+        //         }
+        //     });
+        // }
+    },
+    // mounted() {
+    //     const observer = new IntersectionObserver(this.handleIntersection, {
+    //         threshold: 0 // Adjust this as needed
+    //     });
+
+    //     // Select each child element to be observed
+    //     const elements = document.querySelectorAll('.col-7 h2, .col-7 h4, .col-7 button');
+    //     elements.forEach(element => {
+    //         observer.observe(element);
+    //     });
+    // }
+}
+</script>
+
+<style scoped>
+@import './DiscInsights.scss';
+
+.body {
+    padding: 50px 0;
+    margin-top: 40px;
+    margin-bottom: 100px;
+}
+
+h4 {
+    line-height: 38px;
+}
+
+.col-7 .arrow {
+    width: 5%;
+    margin-top: -40px;
+}
+
+.col-6 img {
+    width: 85%;
+    margin-top: 10px;
+    opacity: 1 !important;
+    transform: none !important;
+}
+
+.button {
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 30px;
+    text-align: left;
+    cursor: pointer;
+    background-color: #00a8ff;
+    min-width: 300px;
+    border-radius: 40px;
+
+}
+
+/* @keyframes rise {
+    from {
+        transform: translateY(1in);
+        opacity: 0;
+    }
+    to {
+        transform: translateY(0);
+        opacity: 1;
+    }
+}
+
+.col-7 h2,
+.col-7 h4,
+.col-7 button {
+    opacity: 0;
+    transform: translateY(1in);
+    transition: all 0.5s ease-out;
+}
+
+.animate-rise {
+    animation: rise 1.25s ease-out forwards;
+} */
+</style>
--- pages/discinsights.vue
+++ pages/discinsights.vue
@@ -2,6 +2,7 @@
     <section>
       <main-nav class="nav"></main-nav>
       <section class="body">
+        <hero-fold></hero-fold>
        <contact-form></contact-form>
       </section>
       <footer-fold class="foot"></footer-fold>
@@ -10,12 +11,14 @@
   
   <script>
   import Nav from '@/components/Nav';
+  import HeroFold from '@/components/discInsights/DiscInsightsHeroFold';
   import ContactForm from '@/components/discInsights/DiscInsightsContactFormFold';
   import Footer from '@/components/Footer';
   
   export default {
     components: {
       'main-nav': Nav,
+      'hero-fold': HeroFold,
       'contact-form': ContactForm,
       'footer-fold': Footer
     },
