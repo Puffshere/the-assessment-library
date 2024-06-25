5e44fab6181482b9b9641b8643cbfd37b2f8534d
Shawn Taylor <shawncurtistaylor@hotmail.com>
2024-06-25 09:17:14 -0700

    puts hard coded phone number back in nav and footer

--- components/Nav.vue
+++ components/Nav.vue
@@ -33,8 +33,9 @@
                                 </div>
 
                                 <div class="phone-number">
-                                    <a class="hyperlink light" href="`tel:${formattedPhoneNumber}`">{{
-                                        formattedPhoneNumber }}</a>
+                                    <a class="hyperlink light" style="text-align: center" href="tel:12064006647">+1 (206) 400-6647</a>
+                                    <!-- <a class="hyperlink light" href="`tel:${formattedPhoneNumber}`">{{
+                                        formattedPhoneNumber }}</a> -->
                                 </div>
                             </div>
                         </div>
