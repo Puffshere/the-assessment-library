7cca2182e8657a677ab9b879bcb016acdace4d61
Shawn Taylor <shawncurtistaylor@hotmail.com>
2024-07-18 01:40:30 -0700

    adds + to phone number and makes http https on the youtube link located on the contact sidebar

--- components/ContactSidebar.vue
+++ components/ContactSidebar.vue
@@ -7,7 +7,7 @@
         </p>
         
         <p class="contact-item">
-            Phone: <a class="hyperlink" href="tel:12064006647">+1 (206) 400-6647</a>
+            Phone: <a class="hyperlink" href="tel:+12064006647">+1 (206) 400-6647</a>
         </p>
 
         <p class="contact-item">
@@ -45,7 +45,7 @@
         </a>
 
         <!-- YouTube icon -->
-        <a class="contact-social" target="_blank" rel="noopener" href="http://www.youtube.com/assessments24x7online" aria-label="Watch Assessments 24x7 on YouTube">
+        <a class="contact-social" target="_blank" rel="noopener" href="https://www.youtube.com/assessments24x7online" aria-label="Watch Assessments 24x7 on YouTube">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                 <path d="M22.54 6.42a2.78 2.78 0 0 0-1.94-2C18.88 4 12 4 12 4s-6.88 0-8.6.46a2.78 2.78 0 0 0-1.94 2A29 29 0 0 0 1 11.75a29 29 0 0 0 .46 5.33A2.78 2.78 0 0 0 3.4 19c1.72.46 8.6.46 8.6.46s6.88 0 8.6-.46a2.78 2.78 0 0 0 1.94-2 29 29 0 0 0 .46-5.25 29 29 0 0 0-.46-5.33z"></path>
                 <polygon points="9.75 15.02 15.5 11.75 9.75 8.48 9.75 15.02"></polygon>
