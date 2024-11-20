42e6b25dae267c90daac3a5197f2180b96780443
Shawn Taylor <shawncurtistaylor@hotmail.com>
2024-11-20 02:03:03 -0800

    working on ios mobile responsiveness for the assessments assessments dev page

--- components/assessments/WhatAre.vue
+++ components/assessments/WhatAre.vue
@@ -104,11 +104,6 @@ section {
 }
 
 @media (max-width: 1200px) {
-    .section {
-        height: auto;
-        margin-bottom: 20px;
-    }
-
     .bottomContainer {
         padding: 20px;
         margin-bottom: 30px;
